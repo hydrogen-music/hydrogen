bec61755f1cf6f7070aa6360ef36a6fd8cb8ae4b
Alessandro Cominu <cominu@gmail.com>
2007-07-21 17:25:05 +0000

    git-svn-id: http://svn.assembla.com/svn/hydrogen/trunk@38 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- docs/docs.h
+++ docs/docs.h
@@ -20,37 +20,18 @@ Table of contents:
 \section sec Scaricare i sorgenti
 Ottenere i sorgenti dal repository subversion:
 \code
-svn co http://involution.dyndns.org/svn/involution_game_studio/game_studio game_studio
+svn co http://hydrogen-music.org/svn/trunk hydrogen_svn
 \endcode
 
 
 \section linux_build Compilazione su Linux
-\subsection linux_ode_build ODE
-\code
-./configure --enable-double-precision --enable-release --disable-shared
-\endcode
+todo: compilazione su linux
 
 \section mac_build Compilazione su Mac OSX
-\todo Inserire come compilare ODE su macosx
-
+todo: compilazione su macosx
 
 \section win_build Compilazione su Windows XP
-\subsection s ODE
-Scaricare e installare "premake" (http://premake.sourceforge.net/)
-\code
-cd ode-0.8/build
-premake --with-tests --target gnu --enable-static-only
-make CONFIG=Release
-\endcode
-\subsection PThreads
-E' necessario scaricare pthreads-w32-2-8-0-release.tar.gz da ftp://sourceware.org/pub/pthreads-win32/
-\code
-cd pthreads-w32-2-8-0-release
-make GC
-\endcode
-
-
-
+todo: compilazione su winxp
 
 
 
