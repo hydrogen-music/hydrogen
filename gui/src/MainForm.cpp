44f36ca8f1d019687317b7249696f7a43a9f677f
Sebastian Moors <mauser@smoors.de>
2008-08-19 23:35:38 +0000

    Bugfix: update SoundlibraryPanel after "save song/save as"
    git-svn-id: http://svn.assembla.com/svn/hydrogen/trunk@343 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- gui/src/MainForm.cpp
+++ gui/src/MainForm.cpp
@@ -496,6 +496,8 @@ void MainForm::action_file_save_as()
 		action_file_save();
 	}
 	h2app->setStatusBarMessage( trUtf8("Song saved."), 10000 );
+	//update SoundlibraryPanel
+	HydrogenApp::getInstance()->getInstrumentRack()->getSoundLibraryPanel()->updateDrumkitList();
 }
 
 
@@ -534,6 +536,8 @@ void MainForm::action_file_save()
 
 		h2app->setStatusBarMessage( trUtf8("Song saved."), 10000 );
 	}
+	//update SoundlibraryPanel
+	HydrogenApp::getInstance()->getInstrumentRack()->getSoundLibraryPanel()->updateDrumkitList();
 }
 
 
@@ -617,6 +621,9 @@ void MainForm::action_file_export_pattern_as()
 		}
 	}
 	h2app->setStatusBarMessage ( trUtf8 ( "Pattern saved." ), 10000 );
+	
+	//update SoundlibraryPanel
+	HydrogenApp::getInstance()->getInstrumentRack()->getSoundLibraryPanel()->updateDrumkitList();
 }
 
 void MainForm::action_file_open() {
