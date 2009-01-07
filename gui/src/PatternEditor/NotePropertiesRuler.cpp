bf886ff7cb637b5c147dcf9ad9bd59a93ec08cb8
Sebastian Moors <mauser@smoors.de>
2009-01-07 14:00:41 +0000

    Bugfix for #51: The background pixmap of the NotePropertiesRuler was not resized after zoom in
    git-svn-id: http://svn.assembla.com/svn/hydrogen/trunk@715 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- gui/src/PatternEditor/NotePropertiesRuler.cpp
+++ gui/src/PatternEditor/NotePropertiesRuler.cpp
@@ -72,6 +72,7 @@ NotePropertiesRuler::NotePropertiesRuler( QWidget *parent, PatternEditorPanel *p
 
 
 
+
 NotePropertiesRuler::~NotePropertiesRuler()
 {
 	//infoLog("DESTROY");
@@ -801,7 +802,9 @@ void NotePropertiesRuler::updateEditor()
 		editorWidth =  20 + MAX_NOTES * m_nGridWidth;
 	}
 	resize( editorWidth, height() );
-
+		
+	delete m_pBackground;
+	m_pBackground = new QPixmap( editorWidth, m_nEditorHeight );
 
 	if ( m_mode == VELOCITY ) {
 		createVelocityBackground( m_pBackground );
