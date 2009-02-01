72be9bb7a8ac999c777835dc1d7b194b5c8bdbd0
Michael Wolkstein <wolke@k-27.de>
2009-02-01 23:53:18 +0000

    2. fix against ticket #73.
    git-svn-id: http://svn.assembla.com/svn/hydrogen/trunk@758 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- gui/src/SongEditor/SongEditor.cpp
+++ gui/src/SongEditor/SongEditor.cpp
@@ -1118,8 +1118,13 @@ void SongEditorPatternList::patternPopup_delete()
 		pEngine->setSelectedPatternNumber( 0 );
 	}
 	else {
-		// there's no patterns..
-		pEngine->setSelectedPatternNumber( -1 );	// cosi' il pattern editor viene costretto ad aggiornarsi
+		// there's no patterns..	
+		Pattern *emptyPattern = Pattern::get_empty_pattern();
+		emptyPattern->set_name( trUtf8("Pattern 1") );
+		emptyPattern->set_category( trUtf8("not_categorized") );
+		pSongPatternList->add( emptyPattern );
+		pEngine->setSelectedPatternNumber( -1 );
+		pEngine->setSelectedPatternNumber( 0 );
 	}
 
 	delete pattern;
