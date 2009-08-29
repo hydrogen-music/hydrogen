7a3c43552d0b726331c9aa16459d31e3689dfe85
Michael Wolkstein <wolke@k-27.de>
2009-08-29 02:18:51 +0000

    bugfix against ticket #116 and #117 ticket #116 now the expanded qtreeview items collapse right. only in some cases they don't collapse as expected. ticket #117 now the audio-file-browser read the environment DESKTOP_SESSION variable. if the env variable is kde, we also check the kde user settings to activate the right mouse clicking. for all other desktops we will use double clicking for browse through directories.
    git-svn-id: http://svn.assembla.com/svn/hydrogen/trunk@1332 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- gui/src/AudioFileBrowser/AudioFileBrowser.cpp
+++ gui/src/AudioFileBrowser/AudioFileBrowser.cpp
@@ -77,7 +77,12 @@ AudioFileBrowser::AudioFileBrowser ( QWidget* pParent )
 
 	playSamplescheckBox->setChecked( Preferences::get_instance()->__playsamplesonclicking );
 
+	//get the kde or gnome environment variable for mouse double or single clicking
+	singleClick = false;
+	getEnvironment();
+
 	connect( tree, SIGNAL( clicked( const QModelIndex&) ), SLOT( clicked( const QModelIndex& ) ) );
+	connect( tree, SIGNAL( doubleClicked( const QModelIndex&) ), SLOT( doubleClicked( const QModelIndex& ) ) );
 	connect( pathLineEdit, SIGNAL( returnPressed() ), SLOT( updateModelIndex() ) );	
 }
 
@@ -88,6 +93,33 @@ AudioFileBrowser::~AudioFileBrowser()
 	Sample *pNewSample = Sample::load( sEmptySampleFilename );
 	AudioEngine::get_instance()->get_sampler()->preview_sample( pNewSample, 100 );
 	INFOLOG ( "DESTROY" );
+}
+
+
+void AudioFileBrowser::getEnvironment()
+{
+	QString desktopSession  = getenv("DESKTOP_SESSION");
+//kde
+	if(desktopSession == "kde"){
+		QFile envfile( QDir::homePath() + "/.kde/share/config/kdeglobals");
+	
+		if (!envfile.open(QIODevice::ReadOnly | QIODevice::Text))
+			return;
+		
+		QTextStream envin( &envfile );
+		while ( !envin.atEnd() ) {
+			QString envline = envin.readLine();
+			if(envline == QString("SingleClick=true") ){
+				singleClick = true;
+				break;
+			}		
+		}
+	}
+
+//for gnome, xfce and all others we use double click as default
+
+	
+	
 }
 
 
@@ -129,6 +161,38 @@ void AudioFileBrowser::updateModelIndex()
 
 
 void AudioFileBrowser::clicked( const QModelIndex& index )
+{
+	QString path = model->filePath( index );
+
+	if( singleClick )
+		browseTree( index );
+
+	if 	(
+		( path.endsWith( ".wav" ) ) ||
+		( path.endsWith( ".WAV" ) ) ||
+		( path.endsWith( ".au" ) ) ||
+		( path.endsWith( ".AU" ) ) ||
+		( path.endsWith( ".aiff" ) ) ||
+		( path.endsWith( ".AIFF" ) ) ||
+		( path.endsWith( ".flac" ) ) ||
+		( path.endsWith( ".FLAC" ) )
+		) {
+		browseTree( index );
+	}
+
+}
+
+
+
+void AudioFileBrowser::doubleClicked( const QModelIndex& index )
+{
+	if(!singleClick)
+		browseTree( index );
+}
+
+
+
+void AudioFileBrowser::browseTree( const QModelIndex& index )
 {
 
 	QString path = model->filePath( index );
@@ -304,8 +368,32 @@ QStringList AudioFileBrowser::selectedFile()
 
 void AudioFileBrowser::on_m_pPathHometoolButton_clicked()
 {
+	QString toremove = "";
+	QString path = pathLineEdit->text();
+
+	while( path != QDir::rootPath() ){
+
+		if ( path.endsWith( '/') ) {
+			toremove = path.section( '/', -2 );
+		}else
+		{
+			toremove = path.section( '/', -1 );
+		}
+		
+		QString updir =  path.replace( toremove, "" );
+		pathLineEdit->setText( updir );
+		tree->setRootIndex( model->index( updir ) );
+		tree->collapse( model->index( updir  ) );
+		tree->setExpanded( model->index(updir), false  );
+
+		path = pathLineEdit->text();
+	}
+
 	pathLineEdit->setText( QDir::homePath() );
+	filelineedit->setText( QDir::homePath() );
 	tree->setRootIndex( model->index( QDir::homePath() ) );
+
+	tree->collapse( model->index( QDir::homePath())  );
 }
 
 
@@ -327,8 +415,10 @@ void AudioFileBrowser::on_m_pPathUptoolButton_clicked()
 	
 	QString updir =  path.replace( toremove, "" );
 	pathLineEdit->setText( updir );
+	filelineedit->setText( updir );
 	tree->setRootIndex( model->index( updir ) );
 	tree->collapse( model->index( updir  ) );
+	tree->setExpanded( model->index(updir), false  );
 }
 
 
