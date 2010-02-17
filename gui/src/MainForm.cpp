a6fd94af82f5fc9f40358c32d4a70695134458dd
Sebastian Moors <mauser@smoors.de>
2010-02-17 21:45:14 +0000

    Bugfix: generate a valid id for new instruments
    git-svn-id: http://svn.assembla.com/svn/hydrogen/branches/0.9.4@1562 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- gui/src/MainForm.cpp
+++ gui/src/MainForm.cpp
@@ -781,7 +781,7 @@ void MainForm::action_instruments_addInstrument()
 	}
 	++nID;
 
-	Instrument *pNewInstr = new Instrument(QString( nID ), "New instrument", new ADSR());
+	Instrument *pNewInstr = new Instrument(QString::number( nID ), "New instrument", new ADSR());
 	pList->add( pNewInstr );
 	
 	#ifdef JACK_SUPPORT
