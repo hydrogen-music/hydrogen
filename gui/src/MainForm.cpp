cb4f2f1f93c53c9e16ab3eaae2c34197ea9cc2bc
Michael Wolkstein <wolke@k-27.de>
2010-02-25 01:23:07 +0000

    fix some illogical director issues. change tag font color to white and gray.
    git-svn-id: http://svn.assembla.com/svn/hydrogen/trunk@1567 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- gui/src/MainForm.cpp
+++ gui/src/MainForm.cpp
@@ -179,6 +179,8 @@ MainForm::MainForm( QApplication *app, const QString& songFilename )
 	
 //beatcouter
 	Hydrogen::get_instance()->setBcOffsetAdjust();
+// director
+	EventQueue::get_instance()->push_event( EVENT_METRONOME, 1 );
 
 }
 
@@ -1124,7 +1126,7 @@ void MainForm::openSongFile( const QString& sFilename )
 	updateRecentUsedSongList();
 	engine->setSelectedPatternNumber( 0 );
 	HydrogenApp::get_instance()->getSongEditorPanel()->updatePositionRuler();
-	EventQueue::get_instance()->push_event( EVENT_METRONOME, 2 );
+	EventQueue::get_instance()->push_event( EVENT_METRONOME, 1 );
 }
 
 
