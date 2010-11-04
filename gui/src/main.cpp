4e7ebecb8ac37c5168607abbd8d974db43ef998f
Sebastian Moors <mauser@smoors.de>
2010-11-04 21:48:17 +0000

    ported the "-k" switch from cli to gui version (Patch supplied by Nick Humphrey)
    git-svn-id: http://svn.assembla.com/svn/hydrogen/trunk@1927 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -64,6 +64,7 @@ static struct option long_opts[] = {
 	{"verbose", optional_argument, NULL, 'V'},
 	{"help", 0, NULL, 'h'},
 	{"install", required_argument, NULL, 'i'},
+	{"drumkit", required_argument, NULL, 'k'},
 	{0, 0, 0, 0},
 };
 
@@ -171,6 +172,7 @@ int main(int argc, char *argv[])
 		bool showVersionOpt = false;
 		const char* logLevelOpt = "Error";
 		QString drumkitName;
+		QString drumkitToLoad;
 		bool showHelpOpt = false;
 
 		int c;
@@ -190,6 +192,11 @@ int main(int argc, char *argv[])
 
 				case 'p':
 					playlistFilename = QString::fromLocal8Bit(optarg);
+					break;
+
+				case 'k':
+					//load Drumkit
+					drumkitToLoad = QString::fromLocal8Bit(optarg);
 					break;
 
 				case 'v':
@@ -254,7 +261,7 @@ int main(int argc, char *argv[])
 		    H2Core::Drumkit::install( drumkitName );
 		    exit(0);
 		}
-
+		
 		if (sSelectedDriver == "auto") {
 			pPref->m_sAudioDriver = "Auto";
 		}
@@ -362,7 +369,12 @@ int main(int argc, char *argv[])
 		{
 			_ERRORLOG ( "Error loading the playlist" );
 		}
-		
+
+		if( ! drumkitToLoad.isEmpty() ){
+        	H2Core::Drumkit* drumkitInfo = H2Core::Drumkit::load( drumkitToLoad );
+            H2Core::Hydrogen::get_instance()->loadDrumkit( drumkitInfo );
+		}
+				
 		pQApp->exec();
 
 		delete pSplash;
@@ -443,3 +455,4 @@ void showUsage()
 	std::cout << "   -v, --version - Show version info" << std::endl;
 	std::cout << "   -h, --help - Show this help message" << std::endl;
 }
+
