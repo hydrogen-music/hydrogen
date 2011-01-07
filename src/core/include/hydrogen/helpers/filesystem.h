b6b160159508442b7600abfde422105edbf0893a
Jeremy Zurcher <jeremy@asynk.ch>
2011-01-07 20:37:54 +0000

    add filesystem helper
    git-svn-id: http://svn.assembla.com/svn/hydrogen/branches/undo@2042 3b3fb362-3133-0410-aa15-cf69e0a59cb7

---
+++ src/core/include/hydrogen/helpers/filesystem.h
@@ -0,0 +1,214 @@
+
+#ifndef H2C_FILESYSTEM_H
+#define H2C_FILESYSTEM_H
+
+#include <hydrogen/object.h>
+#include <QtCore/QString>
+
+namespace H2Core
+{
+
+/**
+ * Filesystem is a thin layer over QDir, QFile and QFileInfo
+ */
+class Filesystem : public Object
+{
+    H2_OBJECT
+    public:
+        /** flags available for Filesystem::check_permissions */
+	    typedef enum _file_perms {
+            is_dir =0x01,
+            is_file=0x02,
+            is_readable=0x04,
+            is_writable=0x08,
+            is_executable=0x10
+        } file_perms_t;
+
+        /**
+         * check user and system filesystem usability
+         * \param logger is a pointer to the logger instance which will be used
+         */
+        static bool bootstrap( Logger* logger );
+
+        /** returns system data path */
+        static QString sys_data_path();
+        /** returns user data path */
+        static QString usr_data_path();
+
+        /** returns system core config path */
+        static QString sys_core_config();
+        /** returns user core config path */
+        static QString usr_core_config();
+        /** returns system gui config path */
+        static QString sys_gui_config();
+        /** returns user gui config path */
+        static QString usr_gui_config();
+        /** returns system empty sample file path */
+        static QString empty_sample();
+        /** returns system empty song file path */
+        static QString empty_song();
+        /** returns system click file path */
+        static QString click_file();
+        /** returns click file path from user directory if exists, otherwise from system */
+        static QString usr_click_file();
+        /** returns the path to the drumkit XSD (xml schema definition) file */
+        static QString drumkit_xsd( );
+        /** returns the path to the pattern XSD (xml schema definition) file */
+        static QString pattern_xsd( );
+
+        /** returns gui image path */
+        static QString img_dir();
+        /** returns internationalization path */
+        static QString i18n_dir();
+        /** returns user songs path */
+        static QString songs_dir();
+        /** returns user patterns path */
+        static QString patterns_dir();
+        /** returns system drumkits path */
+        static QString sys_drumkits_dir();
+        /** returns user drumkits path */
+        static QString usr_drumkits_dir();
+        /** returns user playlist path */
+        static QString playlists_dir();
+        /** returns system demos path */
+        static QString demos_dir();
+        /** returns system xsd path */
+        static QString xsd_dir();
+
+        /* DRUMKIT */
+        /** returns list of usable system drumkits ( see Filesystem::drumkits_list ) */
+        static QStringList sys_drumkits_list( );
+        /** returns list of usable user drumkits ( see Filesystem::drumkits_list ) */
+        static QStringList usr_drumkits_list( );
+        /**
+         * returns true if the drumkit exists within usable system or user drumkits
+         * \param dk_name the drumkit name
+         */
+        static bool drumkit_exists( const QString& dk_name );
+        /**
+         * returns path for a drumkit searching within usable system then user drumkits
+         * \param dk_name the drumkit name
+         */
+        static QString drumkit_path( const QString& dk_name );
+        /**
+         * returns true if the path contains a usable drumkit
+         * \param dk_path the path to the drumkit
+         */
+        static bool drumkit_valid( const QString& dk_path );
+        /**
+         * returns the path to the xml file within a suposed drumkit path
+         * \param dk_path the path to the drumkit
+         */
+        static QString drumkit_file( const QString& dk_path );
+
+        /* PATTERNS */
+        /** returns a list of existing patterns */
+        static QStringList patterns_list( );
+
+        /* SONGS */
+        /** returns a list of existing songs */
+        static QStringList songs_list( );
+        /**
+         * returns true if the song file exists
+         * \param sg_name the song name
+         */
+        static bool song_exists( const QString& sg_name );
+
+        /** send current settings information to logger with INFO severity */
+        static void info();
+
+        /**
+         * returns true if the given path is an existing regular file
+         * \param path the path to the file to check
+         * \param silent output not messages if set to true
+         */
+        static bool file_exists( const QString& path, bool silent=false );
+        /**
+         * returns true if the given path is an existing readable regular file
+         * \param path the path to the file to check
+         * \param silent output not messages if set to true
+         */
+        static bool file_readable( const QString& path, bool silent=false );
+        /**
+         * returns true if the given path is a possibly writable file (may exist or not)
+         * \param path the path to the file to check
+         * \param silent output not messages if set to true
+         */
+        static bool file_writable( const QString& path, bool silent=false );
+        /**
+         * returns true if the given path is an existing executable regular file
+         * \param path the path to the file to check
+         * \param silent output not messages if set to true
+         */
+        static bool file_executable( const QString& path, bool silent=false );
+        /**
+         * returns true if the given path is a readable regular directory
+         * \param path the path to the file to check
+         * \param silent output not messages if set to true
+         */
+        static bool dir_readable( const QString& path, bool silent=false );
+        /**
+         * returns true if the given path is a writable regular directory
+         * \param path the path to the file to check
+         * \param silent output not messages if set to true
+         */
+        static bool dir_writable( const QString& path, bool silent=false );
+        /**
+         * returns true if the path is a readable and writable regular directory, create if it not exists
+         * \param path the path to the file to check
+         * \param silent output not messages if set to true
+         */
+        static bool path_usable( const QString& path, bool silent=false );
+        /**
+         * writes to a file
+         * \param dst the destination path
+         * \param content then string to write
+         */
+        static bool write_to_file( const QString& dst, const QString& content );
+        /**
+         * copy a source file to a destination
+         * \param src source file path
+         * \param dsr destination file path
+         * \param overwrite allow to overwrite an existing file if set to true
+         */
+        static bool file_copy( const QString& src, const QString& dst, bool overwrite=false );
+        /**
+         * remove a path
+         * \param path the path to be removed
+         * \param recursive performe recursive removal if set to true
+         */
+        static bool rm( const QString& path, bool recursive=false );
+        /**
+         * create a path
+         * \param path the path to the directory to be created
+         */
+        static bool mkdir( const QString& path );
+
+    private:
+        static Logger* __logger;                    ///< a pointer to the logger
+        static bool check_sys_paths();              ///< returns true if the system path is consistent
+        static bool check_usr_paths();              ///< returns true if the user path is consistent
+        static bool rm_fr( const QString& path );   ///< recursively remove a path
+
+        /**
+         * returns a list of usable drumkits, which means having a readable drumkit.xml file
+         * \param path the path to search in for drumkits
+         */
+        static QStringList drumkits_list( const QString& path );
+        /**
+         * return true if all the asked permissions are ok
+         * \param path the path to the file to check
+         * \param perms bit mask of file_perms_t
+         * \param silent output not messages if set to true
+         */
+        static bool check_permissions( const QString& path, const int perms, bool silent );
+
+        static QString __sys_data_path;     ///< the path to the system files
+        static QString __usr_data_path;     ///< the path to the user files
+};
+
+};
+
+#endif  // H2C_FILESYSTEM_H
+
+/* vim: set softtabstop=4 expandtab: */
---
+++ src/core/src/helpers/filesystem.cpp
@@ -0,0 +1,383 @@
+
+#include <hydrogen/config.h>
+#include <hydrogen/helpers/filesystem.h>
+
+#include <QtCore/QDir>
+#include <QtCore/QFile>
+#include <QtCore/QFileInfo>
+#include <QtCore/QCoreApplication>
+
+// directories
+#define IMG             "/img"
+#define I18N            "/i18n"
+#define SONGS           "/songs"
+#define PATTERNS        "/patterns"
+#define DRUMKITS        "/drumkits"
+#define PLAYLISTS       "/playlists"
+#define DEMOS           "/demo_songs"
+#define XSD             "/xsd"
+
+// files
+#define GUI_CONFIG      "/gui.conf"
+#define CORE_CONFIG     "/core.conf"
+#define CLICK_SAMPLE    "/click.wav"
+#define EMPTY_SAMPLE    "/empty_sample.wav"
+#define EMPTY_SONG      "/empty_song.h2song"
+
+// filters
+#define SONG_FILTER     "*.h2song"
+#define PATTERN_FILTER  "*.h2pattern"
+#define DRUMKIT_XML     "drumkit.xml"
+#define DRUMKIT_XSD     "drumkit.xsd"
+#define PATTERN_XSD     "pattern.xsd"
+
+namespace H2Core {
+
+Logger* Filesystem::__logger = 0;
+const char* Filesystem::__class_name = "Filesystem";
+QString Filesystem::__sys_data_path;
+QString Filesystem::__usr_data_path;
+
+/* TODO QCoreApplication is not instanciated */
+bool Filesystem::bootstrap( Logger* logger ) {
+    if( __logger==0 && logger!=0 ) {
+        __logger = logger;
+    } else {
+        return false;
+    }
+#ifdef Q_OS_MACX
+#ifdef H2CORE_HAVE_BUNDLE
+    //Bundle: Prepare hydrogen to use path names which are used in app bundles: http://en.wikipedia.org/wiki/Application_Bundle
+    __sys_data_path = QCoreApplication->applicationDirPath().append( "/../Resources/data" ) ;
+#else
+    __sys_data_path = QCoreApplication->applicationDirPath().append( "/data" ) ;
+#endif
+    __usr_data_path = QDir::homePath().append( "/Library/Application Support/Hydrogen" );
+#elif WIN32
+    __sys_data_path = QCoreApplication->applicationDirPath().append( "/data" ) ;
+    __usr_data_path = QCoreApplication->applicationDirPath().append( "/hydrogen/data" ) ;
+#else
+    __sys_data_path = SYS_DATA_PATH;
+    __usr_data_path = QDir::homePath().append( "/"USR_DATA_PATH );
+#endif
+    return check_sys_paths() && check_usr_paths();
+}
+
+bool Filesystem::check_permissions( const QString& path, const int perms, bool silent ) {
+    QFileInfo fi( path );
+    if( ( perms & is_file ) && ( perms & is_writable ) && !fi.exists() ) {
+        QFileInfo folder( path.left( path.lastIndexOf( "/" ) ) );
+        if( !folder.isDir() ) {
+            if( !silent ) ERRORLOG( QString( "%1 is not a directory" ).arg( folder.fileName() ) );
+            return false;
+        }
+        if( !folder.isWritable() ) {
+            if( !silent ) ERRORLOG( QString( "%1 is not writable" ).arg( folder.fileName() ) );
+            return false;
+        }
+        return true;
+    }
+    if( ( perms & is_dir ) && !fi.isDir() ) {
+        if( !silent ) ERRORLOG( QString( "%1 is not a directory" ).arg( path ) );
+        return false;
+    }
+    if( ( perms & is_file ) && !fi.isFile() ) {
+        if( !silent ) ERRORLOG( QString( "%1 is not a file" ).arg( path ) );
+        return false;
+    }
+    if( ( perms & is_readable ) && !fi.isReadable() ) {
+        if( !silent ) ERRORLOG( QString( "%1 is not readable" ).arg( path ) );
+        return false;
+    }
+    if( ( perms & is_writable ) && !fi.isWritable() ) {
+        if( !silent ) ERRORLOG( QString( "%1 is not writable" ).arg( path ) );
+        return false;
+    }
+    if( ( perms & is_executable ) && !fi.isExecutable() ) {
+        if( !silent ) ERRORLOG( QString( "%1 is not executable" ).arg( path ) );
+        return false;
+    }
+    return true;
+}
+
+bool Filesystem::file_exists( const QString& path, bool silent ) {
+    return check_permissions( path, is_file, silent );
+}
+bool Filesystem::file_readable( const QString& path, bool silent ) {
+    return check_permissions( path, is_file|is_readable, silent );
+}
+bool Filesystem::file_writable( const QString& path, bool silent ) {
+    return check_permissions( path, is_file|is_writable, silent );
+}
+bool Filesystem::file_executable( const QString& path, bool silent ) {
+    return check_permissions( path, is_file|is_executable, silent );
+}
+bool Filesystem::dir_readable(  const QString& path, bool silent ) {
+    return check_permissions( path, is_dir|is_readable|is_executable, silent );
+}
+bool Filesystem::dir_writable(  const QString& path, bool silent ) {
+    return check_permissions( path, is_dir|is_writable, silent );
+}
+
+bool Filesystem::mkdir( const QString& path ) {
+    if ( !QDir( "/" ).mkpath( QDir( path ).absolutePath() ) ) {
+        ERRORLOG( QString( "unable to create directory : %1" ).arg( path ) );
+        return false;
+    }
+    return true;
+}
+
+bool Filesystem::path_usable( const QString& path, bool silent ) {
+    if( !QDir( path ).exists() ) {
+        if( !silent ) INFOLOG( QString( "create user directory : %1" ).arg( path ) );
+        if( !QDir( "/" ).mkpath( path ) ) {
+            if( !silent ) ERRORLOG( QString( "unable to create user directory : %1" ).arg( path ) );
+            return false;
+        }
+    }
+    return dir_readable( path, silent ) && dir_writable( path, silent );
+}
+
+bool Filesystem::write_to_file( const QString& dst, const QString& content ) {
+    if ( !file_writable( dst ) ) {
+        ERRORLOG( QString( "unable to write to %1" ).arg( dst ) );
+        return false;
+    }
+    QFile file( dst );
+    if ( !file.open( QIODevice::WriteOnly ) ) {
+        ERRORLOG( QString( "unable to write to %1" ).arg( dst ) );
+        return false;
+    }
+    file.write( content.toUtf8().data() );
+    file.close();
+}
+
+bool Filesystem::file_copy( const QString& src, const QString& dst, bool overwrite ) {
+    if( file_exists( dst, true ) && !overwrite ) {
+        WARNINGLOG( QString( "do not overwrite %1 with %2 has it already exists" ).arg( dst ).arg( src ) );
+        return true;
+    }
+    if ( !file_readable( src ) ) {
+        ERRORLOG( QString( "unable to copy %1 to %2, %1 is not readable" ).arg( src ).arg( dst ) );
+        return false;
+    }
+    if ( !file_writable( dst ) ) {
+        ERRORLOG( QString( "unable to copy %1 to %2, %2 is not writable" ).arg( src ).arg( dst ) );
+        return false;
+    }
+    INFOLOG( QString( "copy %1 to %2" ).arg( src ).arg( dst ) );
+    return QFile::copy( src,dst );
+}
+
+bool Filesystem::rm( const QString& path, bool recursive ) {
+    if ( check_permissions( path, is_file, true ) ) {
+        QFile file( path );
+        bool ret = file.remove();
+        if( !ret ) {
+            ERRORLOG( QString( "unable to remove file %1" ).arg( path ) );
+        }
+        return ret;
+    }
+    if ( !check_permissions( path, is_dir, true )  ) {
+        ERRORLOG( QString( "%1 is neither a file nor a directory ?!?!" ).arg( path ) );
+        return false;
+    }
+    if ( !recursive ) {
+        ERRORLOG( QString( "unable to remove directory %1 without recursive parameter set to true" ).arg( path ) );
+        return false;
+    }
+    return rm_fr( path );
+}
+
+bool Filesystem::rm_fr( const QString& path ) {
+    bool ret = true;
+    QDir dir( path );
+    QFileInfoList entries = dir.entryInfoList( QDir::NoDotAndDotDot | QDir::AllEntries );
+    for ( int idx = 0; ( ( idx < entries.size() ) && ret ); idx++ ) {
+        QFileInfo entryInfo = entries[idx];
+        if ( entryInfo.isDir() && !entryInfo.isSymLink() ) {
+            ret = rm_fr( entryInfo.absoluteFilePath() );
+        } else {
+            QFile file( entryInfo.absoluteFilePath() );
+            if ( !file.remove() ) {
+                ERRORLOG( QString( "unable to remove %1" ).arg( entryInfo.absoluteFilePath() ) );
+                ret = false;
+            }
+        }
+    }
+    if ( !dir.rmdir( dir.absolutePath() ) ) {
+        ERRORLOG( QString( "unable to remove %1" ).arg( dir.absolutePath() ) );
+        ret = false;
+    }
+    return ret;
+}
+
+bool Filesystem::check_sys_paths() {
+    if(  !dir_readable( __sys_data_path ) ) return false;
+    if(  !dir_readable( img_dir() ) ) return false;
+    if(  !dir_readable( xsd_dir() ) ) return false;
+    if(  !dir_readable( i18n_dir() ) ) return false;
+    if(  !dir_readable( demos_dir() ) ) return false;
+    if( !file_readable( click_file() ) ) return false;
+    if( !file_readable( empty_song() ) ) return false;
+    if( !file_readable( empty_sample() ) ) return false;
+    if( !file_readable( sys_gui_config() ) ) return false;
+    if( !file_readable( sys_core_config() ) ) return false;
+    if(  !dir_readable( sys_drumkits_dir() ) ) return false;
+    if( !file_readable( drumkit_xsd() ) ) return false;
+    if( !file_readable( pattern_xsd() ) ) return false;
+    INFOLOG( QString( "system wide data path %1 is usable." ).arg( __sys_data_path ) );
+    return true;
+}
+
+
+bool Filesystem::check_usr_paths() {
+    if( !path_usable( __usr_data_path ) ) return false;
+    if( !path_usable( songs_dir() ) ) return false;
+    if( !path_usable( patterns_dir() ) ) return false;
+    if( !path_usable( playlists_dir() ) ) return false;
+    if( !path_usable( usr_drumkits_dir() ) ) return false;
+    INFOLOG( QString( "user path %1 is usable." ).arg( __usr_data_path ) );
+    return true;
+}
+
+QString Filesystem::sys_data_path() {
+    return __sys_data_path;
+}
+QString Filesystem::usr_data_path() {
+    return __usr_data_path;
+}
+
+// FILES
+QString Filesystem::sys_core_config() {
+    return __sys_data_path + CORE_CONFIG;
+}
+QString Filesystem::usr_core_config() {
+    return __usr_data_path + CORE_CONFIG;
+}
+QString Filesystem::sys_gui_config() {
+    return __sys_data_path + GUI_CONFIG;
+}
+QString Filesystem::usr_gui_config() {
+    return __usr_data_path + GUI_CONFIG;
+}
+QString Filesystem::empty_sample() {
+    return __sys_data_path + EMPTY_SAMPLE;
+}
+QString Filesystem::empty_song() {
+    return __sys_data_path + EMPTY_SONG;
+}
+QString Filesystem::click_file() {
+    return __sys_data_path + CLICK_SAMPLE;
+}
+QString Filesystem::usr_click_file() {
+    if( file_readable( __usr_data_path + CLICK_SAMPLE, true ) ) return __usr_data_path + CLICK_SAMPLE;
+    return click_file();
+}
+QString Filesystem::drumkit_xsd( ) {
+    return xsd_dir() + "/" + DRUMKIT_XSD;
+}
+QString Filesystem::pattern_xsd( ) {
+    return xsd_dir() + "/" + PATTERN_XSD;
+}
+
+// DIRS
+QString Filesystem::img_dir() {
+    return __sys_data_path + IMG;
+}
+QString Filesystem::i18n_dir() {
+    return __sys_data_path + I18N;
+}
+QString Filesystem::songs_dir() {
+    return __usr_data_path + SONGS;
+}
+QString Filesystem::patterns_dir() {
+    return __usr_data_path + PATTERNS;
+}
+QString Filesystem::sys_drumkits_dir() {
+    return __sys_data_path + DRUMKITS;
+}
+QString Filesystem::usr_drumkits_dir() {
+    return __usr_data_path + DRUMKITS;
+}
+QString Filesystem::playlists_dir(){
+    return __usr_data_path + PLAYLISTS;
+}
+QString Filesystem::demos_dir() {
+    return __sys_data_path + DEMOS;
+}
+QString Filesystem::xsd_dir() {
+    return __sys_data_path + XSD;
+}
+
+// DRUMKITS
+QStringList Filesystem::drumkits_list( const QString& path ) {
+    QStringList ok;
+    QStringList possible = QDir( path ).entryList( QDir::Dirs | QDir::NoDotAndDotDot );
+    for( int i=0; i<possible.size(); i++ ) {
+        if ( file_readable( path+"/"+possible[i]+"/"+DRUMKIT_XML, true ) )
+            ok << possible[i];
+        else {
+            ERRORLOG( QString( "drumkit %1 is not usable" ).arg( path+"/"+possible[i] ) );
+        }
+    }
+    return ok;
+}
+QStringList Filesystem::sys_drumkits_list( ) {
+    return drumkits_list( sys_drumkits_dir() ) ;
+}
+QStringList Filesystem::usr_drumkits_list( ) {
+    return drumkits_list( usr_drumkits_dir() ) ;
+}
+bool Filesystem::drumkit_exists( const QString& dk_name ) {
+    if( sys_drumkits_list().contains( dk_name ) ) return true;
+    return usr_drumkits_list().contains( dk_name );
+}
+QString Filesystem::drumkit_path( const QString& dk_name ) {
+    if( sys_drumkits_list().contains( dk_name ) ) return sys_drumkits_dir() + "/" + dk_name;
+    if( usr_drumkits_list().contains( dk_name ) ) return usr_drumkits_dir() + "/" + dk_name;
+    ERRORLOG( QString( "drumkit %1 not found" ).arg( dk_name ) );
+    return "";
+}
+bool Filesystem::drumkit_valid( const QString& dk_path ) {
+    return file_readable( dk_path + "/" + DRUMKIT_XML );
+}
+QString Filesystem::drumkit_file( const QString& dk_path ) {
+    return dk_path + "/" + DRUMKIT_XML;
+}
+
+// PATTERNS
+QStringList Filesystem::patterns_list( ) {
+    return QDir( patterns_dir() ).entryList( QStringList( PATTERN_FILTER ), QDir::Files | QDir::NoDotAndDotDot );
+}
+
+// SONGS
+QStringList Filesystem::songs_list( ) {
+    return QDir( songs_dir() ).entryList( QStringList( SONG_FILTER ), QDir::Files | QDir::NoDotAndDotDot );
+}
+bool Filesystem::song_exists( const QString& sg_name ) {
+    return QDir( songs_dir() ).exists( sg_name );
+}
+
+void Filesystem::info() {
+    INFOLOG( QString( "Images dir                 : %1" ).arg( img_dir() ) );
+    INFOLOG( QString( "Internationalization dir   : %1" ).arg( i18n_dir() ) );
+    INFOLOG( QString( "Demos dir                  : %1" ).arg( demos_dir() ) );
+    INFOLOG( QString( "XSD dir                    : %1" ).arg( xsd_dir() ) );
+    INFOLOG( QString( "System drumkit dir         : %1" ).arg( sys_drumkits_dir() ) );
+    INFOLOG( QString( "System wide core cfg file  : %1" ).arg( sys_core_config() ) );
+    INFOLOG( QString( "System wide gui cfg file   : %1" ).arg( sys_gui_config() ) );
+    INFOLOG( QString( "Empty sample               : %1" ).arg( empty_sample() ) );
+    INFOLOG( QString( "Empty song                 : %1" ).arg( empty_song() ) );
+    INFOLOG( QString( "Click file                 : %1" ).arg( click_file() ) );
+    INFOLOG( QString( "User drumkit dir           : %1" ).arg( usr_drumkits_dir() ) );
+    INFOLOG( QString( "Songs dir                  : %1" ).arg( songs_dir() ) );
+    INFOLOG( QString( "Patterns dir               : %1" ).arg( patterns_dir() ) );
+    INFOLOG( QString( "Playlists dir              : %1" ).arg( playlists_dir() ) );
+    INFOLOG( QString( "User core cfg file         : %1" ).arg( usr_core_config() ) );
+    INFOLOG( QString( "User gui cfg file          : %1" ).arg( usr_gui_config() ) );
+}
+
+};
+
+/* vim: set softtabstop=4 expandtab: */
