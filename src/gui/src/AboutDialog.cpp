e9410b2f76570b94c70575ad9cc909645cb67c65
Sebastian Moors <mauser@smoors.de>
2011-11-29 23:07:08 +0000

    Clean up aboutdialog: No need to use a QTextEdit for displaying text
    git-svn-id: http://svn.assembla.com/svn/hydrogen/trunk@2389 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- src/gui/src/AboutDialog.cpp
+++ src/gui/src/AboutDialog.cpp
@@ -121,7 +121,7 @@ AboutDialog::AboutDialog(QWidget* parent)
 	sAuthors += "</ul>";
 
 
-	authorsTxt->append( sAuthors );
+	authorsTxt->setText( sAuthors );
 
 	logoLabel->setPixmap( QPixmap( Skin::getImagePath() +"/about/aboutLogo.png" ) );
 }
