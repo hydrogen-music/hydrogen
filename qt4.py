3a66f3a9dc0e593d254a0956f3951231019963f9
Sebastian Moors <mauser@smoors.de>
2012-01-21 12:06:58 +0000

    [0.9.5] fixed compilation issue in qt4.py on linux3
    git-svn-id: http://svn.assembla.com/svn/hydrogen/branches/0.9.5@2443 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- qt4.py
+++ qt4.py
@@ -444,7 +444,7 @@ def enable_modules(self, modules, debug=False, crosscompiling=False) :
 		try : self.AppendUnique(CPPDEFINES=moduleDefines[module])
 		except: pass
 	debugSuffix = ''
-	if sys.platform == "linux2" and not crosscompiling :
+	if sys.platform.startswith("linux") and not crosscompiling :
 		if debug : debugSuffix = '_debug'
 		for module in modules :
 			if module not in pclessModules : continue
