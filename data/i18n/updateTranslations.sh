e8bc9a6d0f02c78300ae776fe80a0e0612f61aa1
Sebastian Moors <mauser@smoors.de>
2012-11-22 22:23:59 +0100

    Bugfix for translation script

--- data/i18n/updateTranslations.sh
+++ data/i18n/updateTranslations.sh
@@ -23,20 +23,20 @@ CMD="$LUPDATE -noobsolete ${FILES} -ts"
 
 
 
-$CMD ../data/i18n/hydrogen.it.ts
-$CMD ../data/i18n/hydrogen.es.ts
-$CMD ../data/i18n/hydrogen.ru.ts
-$CMD ../data/i18n/hydrogen.fr.ts
-$CMD ../data/i18n/hydrogen.pt_BR.ts
-$CMD ../data/i18n/hydrogen.hu_HU.ts
-$CMD ../data/i18n/hydrogen.pl.ts
-$CMD ../data/i18n/hydrogen.nl.ts
-$CMD ../data/i18n/hydrogen.ja.ts
-$CMD ../data/i18n/hydrogen.de.ts
-$CMD ../data/i18n/hydrogen.sv.ts
-$CMD ../data/i18n/hydrogen.hr.ts
-$CMD ../data/i18n/hydrogen.cs.ts
-$CMD ../data/i18n/hydrogen.el.ts
+$CMD ../../data/i18n/hydrogen.it.ts
+$CMD ../../data/i18n/hydrogen.es.ts
+$CMD ../../data/i18n/hydrogen.ru.ts
+$CMD ../../data/i18n/hydrogen.fr.ts
+$CMD ../../data/i18n/hydrogen.pt_BR.ts
+$CMD ../../data/i18n/hydrogen.hu_HU.ts
+$CMD ../../data/i18n/hydrogen.pl.ts
+$CMD ../../data/i18n/hydrogen.nl.ts
+$CMD ../../data/i18n/hydrogen.ja.ts
+$CMD ../../data/i18n/hydrogen.de.ts
+$CMD ../../data/i18n/hydrogen.sv.ts
+$CMD ../../data/i18n/hydrogen.hr.ts
+$CMD ../../data/i18n/hydrogen.cs.ts
+$CMD ../../data/i18n/hydrogen.el.ts
 
 echo "Creating *.qm files"
 cd ../../data/i18n
