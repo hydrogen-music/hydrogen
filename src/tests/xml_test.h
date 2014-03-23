7005f9163cf72b5d00d3dfeb1026b18a7d84dc93
Przemysław Sitek <przemyslaw.sitek@gmail.com>
2014-03-23 17:39:54 +0100

    Migrate tests to CppUnit

---
+++ src/tests/xml_test.h
@@ -0,0 +1,20 @@
+#ifndef XML_TEST_H
+#define XML_TEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class XmlTest : public CppUnit::TestCase {
+	CPPUNIT_TEST_SUITE(XmlTest);
+	CPPUNIT_TEST(testDrumkit);
+	CPPUNIT_TEST(testPattern);
+	CPPUNIT_TEST_SUITE_END();
+
+	public:
+	virtual void setUp();
+
+	void testDrumkit();
+	void testPattern();
+};
+
+
+#endif
