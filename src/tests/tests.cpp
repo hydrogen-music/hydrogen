6649c1c42e07682789c2a62326a98a468c4e1d60
Sebastian Moors <mauser@smoors.de>
2014-03-23 17:05:17 +0100

    Fixed #131: Fixed segfault in tests
    The drumkit pattern xsd file still fails to validate with the QT xml parser, but it seems to be fine with xmllint..

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -9,21 +9,21 @@ int xml_pattern( int log_level );
 
 int main( int argc, char* argv[] )
 {
-    int log_level = H2Core::Logger::Debug | H2Core::Logger::Info | H2Core::Logger::Warning | H2Core::Logger::Error;
-    /* Logger */
-    H2Core::Logger* logger = H2Core::Logger::bootstrap( log_level );
-    /* Object */
-    H2Core::Object::bootstrap( logger, logger->should_log( H2Core::Logger::Debug ) );
-    /* Filesystem */
-    H2Core::Filesystem::bootstrap( logger, "./data" );
-    H2Core::Filesystem::info();
-    H2Core::Filesystem::rm( H2Core::Filesystem::tmp_dir(), true );
+	int log_level = H2Core::Logger::Debug | H2Core::Logger::Info | H2Core::Logger::Warning | H2Core::Logger::Error;
+	/* Logger */
+	H2Core::Logger* logger = H2Core::Logger::bootstrap( log_level );
+	/* Object */
+	H2Core::Object::bootstrap( logger, logger->should_log( H2Core::Logger::Debug ) );
+	/* Filesystem */
+	H2Core::Filesystem::bootstrap( logger, "./data" );
+	H2Core::Filesystem::info();
+	H2Core::Filesystem::rm( H2Core::Filesystem::tmp_dir(), true );
 
-    rubberband_test( H2Core::Filesystem::drumkit_path_search( "GMkit" )+"/cym_Jazz.flac" );
-    xml_drumkit( log_level );
-    xml_pattern( log_level );
+	rubberband_test( H2Core::Filesystem::drumkit_path_search( "GMkit" )+"/cym_Jazz.flac" );
+	xml_drumkit( log_level );
+	xml_pattern( log_level );
 
-    delete logger;
+	delete logger;
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
