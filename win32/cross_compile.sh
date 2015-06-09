a87aa7eaa6aa3ba870590a363e90aaf8e7c4eefa
mikotoiii <boyer.steve@gmail.com>
2015-06-09 12:09:13 -0300

    Changed how Windows package is build (From manual NSIS to CPack) and updated the cross_compile.sh script accordingly. Added 64 bit compilation of Hydrogen, and checks for 32 vs 64 bit jack, and libjack(64).dll

--- win32/cross_compile.sh
+++ win32/cross_compile.sh
@@ -4,6 +4,121 @@
 # clear the screen
 clear
 
+build_hydrogen(){
+	# Passes either i686 or x86_64 for 32 or 64 bit respectively.
+	echo "Now starting the building of Hydrogen for Windows. This will take quite a while and requires no interaction after the intial questions."
+	if [ -z ${CLONEPATH%/*} ]; then
+		read -e -p "Enter the path to the Hydrogen download: " -i "$HOME/build/hydrogen/" CLONEPATH
+	fi
+	HYDROGEN="${CLONEPATH%/*}/source"
+	echo "Checking for MXE."
+	if [ ! "$MXE_INSTALLED" == "1" ]; then
+		if [ -z "$MXE" ]; then
+			MXE="${CLONEPATH%/*}/mxe"
+		fi
+		# Ask if MXE should be installed
+		echo "MXE was not found. We will now build MXE."
+		echo "Would you like to permenantly install MXE into /opt/mxe to save time for future builds? (HIGHLY RECOMMENDED)"
+		while true; do
+		    read -p "Do you wish to install this program? (y / n)" yn
+		    case $yn in
+			[Yy]* ) #move mxe files to /opt and build it there!
+				sudo mv $MXE /opt/mxe
+				cd /opt/mxe
+				MXE=/opt/mxe
+				PATH=/opt/mxe/usr/bin:$PATH;
+				break;;
+			[Nn]* ) exit;;
+			* ) echo "Please answer yes or no.";;
+		    esac
+		done
+			#Modify the MXE Makefiles to allow for the cross compilation.
+		sed -i 's/i686-w64-mingw32.static/i686-w64-mingw32.shared x86_64-w64-mingw32.shared/g' $MXE/Makefile
+		cd $MXE
+		#Make gcc and winpthreads. gcc will need to be rebuilt once winpthreads is built.
+		#Note: This needs to happen because winpthreads needs gcc to be built, but we need gcc built with winpthreads support to build hydrogen.
+		#	this creates a cyclical dependancy problem that there seems to be no way around.
+		make gcc
+		make winpthreads
+		sed -i 's/binutils gcc-gmp gcc-isl gcc-mpc gcc-mpfr mingw-w64/binutils gcc-gmp gcc-isl gcc-mpc gcc-mpfr winpthreads/g' $MXE/src/gcc.mk
+		sed -i 's/--enable-threads=win32/--enable-threads=posix/g' $MXE/src/gcc.mk
+		make gcc
+		#Build the dependancies for hydrogen
+		make qt libarchive libsndfile portaudio portmidi fftw rubberband -j4 JOBS=4
+	else
+		echo "MXE found at $MXE"
+	fi
+		#Build hydrogen itself now.
+	echo "Now building Hydrogen."
+	HYDROGEN_BUILD="$HYDROGEN/windows"
+	if [ ! -e "$HYDROGEN_BUILD" ]; then
+		mkdir "$HYDROGEN/windows"
+	fi
+	echo "We will now build Hydrogen at $HYDROGEN_BUILD"
+	cd "$HYDROGEN_BUILD"
+	if [ ! -e "$MXE/usr/$1-w64-mingw32.shared/bin/libjack.dll" ] || [ ! -e "$MXE/usr/$1-w64-mingw32.shared/bin/libjack64.dll" ]; then
+		while true; do	
+			read -p "would you like to build Hydrogen with Jack support? (y / n) Note: If you have libjack.dll or libjack64.dll already installed in mxe, you can answer no here." yn
+			case $yn in
+				[Yy]* ) echo "You will now need to copy the libjack.dll file from a Windows machine (C:\Windows\SysWow64\libjack.dll) to your mxe directory.";
+					echo "If you're building 64 bit, you will need to point the installer to libjack64.dll which can be found at (C:\Windows).
+					echo "We can try to automatically move the file to the proper location" 
+					read -e -p "Enter the path to libjack.dll: " -i "$HOME" LIBJACK_PATH
+					if [ -f $LIBJACK_PATH ]; then
+						sudo cp $LIBJACK_PATH $MXE/usr/$1-w64-mingw32.shared/bin/	
+					fi
+					if [ ! -e $MXE/usr/$1-w64-mingw32.shared/include/jack ]; then
+						ln -s ${CLONEPATH%/*}/jack2/common/jack $MXE/usr/$1-w64-mingw32.shared/include/jack
+					fi
+					break;;
+				[Nn]* ) break;;
+				* ) echo "Please answer yes or no.";;
+			esac
+		done
+	else
+		echo "libjack was found at $MXE/usr/$1-w64-mingw32.shared/bin/"
+	fi
+	cd ../..
+	cmake source -DCMAKE_TOOLCHAIN_FILE=$MXE/usr/$1-w64-mingw32.shared/share/cmake/mxe-conf.cmake
+	#make
+	export HYDROGEN
+	export HYDROGEN_BUILD
+	export MXE
+	if [ ! -e jack_installer ]; then
+		mkdir jack_installer
+	fi
+	cd jack_installer
+	if [ $1 == "x86_64" ]
+		if [ ! -e "Jack_v1.9.10_64_setup.exe" ]; then
+			wget https://dl.dropboxusercontent.com/u/28869550/Jack_v1.9.10_64_setup.exe
+		fi
+	else
+		if [ ! -e "Jack_v1.9.10_32_setup.exe" ]; then
+			wget https://dl.dropboxusercontent.com/u/28869550/Jack_v1.9.10_32_setup.exe
+		fi
+	fi
+	cd ..
+	if [ ! -e plugins ]; then
+		mkdir plugins
+		cd plugins
+		if [ ! -e ladspaplugs ]; then
+			mkdir ladspaplugs
+		fi
+		cd ladspaplugs
+		if [ ! -e "LADSPA_plugins-win-0.4.15.exe" ]; then
+			wget http://downloads.sourceforge.net/audacity/LADSPA_plugins-win-0.4.15.exe
+		fi
+		cd ..
+	fi
+	cd ..
+	cpack -G NSIS
+	#Check if PERMENANT_INSTALL is set to 1, and copy the files over
+	if [ "$PERMENANT_INSTALL" == "1" ]; then
+		sudo mv $MXE /opt/mxe
+	fi
+}
+
+
 while :
 	do
 	# If error exists, display it
@@ -33,6 +148,7 @@ while :
 	# Clear the error message
 	ERR_MSG=""
 
+	
 	# Read the user input
 	read SEL
 
@@ -46,7 +162,7 @@ while :
 			;;
 		2)	#download the proper git repositories
 			echo "This will clone the repositories for Hydrogen and MXE."
-			read -e -p "Enter the path where Hydrogen should be built: " -i "$HOME/build/hydrogen_win32/" CLONEPATH
+			read -e -p "Enter the path where Hydrogen should be built: " -i "$HOME/build/hydrogen/" CLONEPATH
 			if [ ! -e "${CLONEPATH%/*}" ]; then
 				echo "Now downloading Hydrogen."
 				mkdir -p "${CLONEPATH%/*}"
@@ -69,102 +185,15 @@ while :
 			fi
 			;;
 		3)	#Set the required variables
-			echo "Now starting the building of Hydrogen for Windows. This will take quite a while and requires no interaction after the intial questions."
-			if [ -z ${CLONEPATH%/*} ]; then
-				read -e -p "Enter the path to the Hydrogen download: " -i "$HOME/build/hydrogen_win32/" CLONEPATH
-			fi
-			HYDROGEN="${CLONEPATH%/*}/hydrogen"
-			echo "Checking for MXE."
-			if [ ! "$MXE_INSTALLED" == "1" ]; then
-				if [ -z "$MXE" ]; then
-					MXE="${CLONEPATH%/*}/mxe"
-				fi
-				# Ask if MXE should be installed
-				echo "MXE was not found. We will now build MXE."
-				echo "Would you like to permenantly install MXE into /opt/mxe to save time for future builds? (HIGHLY RECOMMENDED)"
-				while true; do
-				    read -p "Do you wish to install this program? (y / n)" yn
-				    case $yn in
-					[Yy]* ) #move mxe files to /opt and build it there!
-						sudo mv $MXE /opt/mxe
-						cd /opt/mxe
-						MXE=/opt/mxe
-						PATH=/opt/mxe/usr/bin:$PATH;
-						break;;
-					[Nn]* ) exit;;
-					* ) echo "Please answer yes or no.";;
-				    esac
-				done
-
-				#Modify the MXE Makefiles to allow for the cross compilation.
-				sed -i 's/i686-w64-mingw32.static/i686-w64-mingw32.shared x86_64-w64-mingw32.shared/g' $MXE/Makefile
-				cd $MXE
-				#Make gcc and winpthreads. gcc will need to be rebuilt once winpthreads is built.
-				#Note: This needs to happen because winpthreads needs gcc to be built, but we need gcc built with winpthreads support to build hydrogen.
-				#	this creates a cyclical dependancy problem that there seems to be no way around.
-				make gcc
-				make winpthreads
-				sed -i 's/binutils gcc-gmp gcc-isl gcc-mpc gcc-mpfr mingw-w64/binutils gcc-gmp gcc-isl gcc-mpc gcc-mpfr winpthreads/g' $MXE/src/gcc.mk
-				sed -i 's/--enable-threads=win32/--enable-threads=posix/g' $MXE/src/gcc.mk
-				make gcc
-				#Build the dependancies for hydrogen
-				make qt libarchive libsndfile portaudio portmidi fftw rubberband -j4 JOBS=4
-			else
-				echo "MXE found at $MXE"
-			fi
-
-			#Build hydrogen itself now.
-			echo "Now building Hydrogen."
-			HYDROGEN_BUILD="$HYDROGEN/win32/windows_32_bit_build"
-			if [ ! -e "$HYDROGEN_BUILD" ]; then
-				mkdir "$HYDROGEN/win32/windows_32_bit_build"
-			fi
-			echo "We will now build Hydrogen at $HYDROGEN_BUILD"
-			cd "$HYDROGEN_BUILD"
-			if [ ! -e "$MXE/usr/i686-w64-mingw32.shared/bin/libjack.dll" ]; then
-				while true; do	
-					read -p "would you like to build Hydrogen with Jack support? (y / n) Note: If you have libjack.dll already installed in mxe, you can answer no here." yn
-					case $yn in
-						[Yy]* ) echo "You will now need to copy the libjack.dll file from a Windows machine (C:\Windows\SysWow64\libjack.dll) to your mxe directory."; 
-							echo "We can try to automatically move the file to the proper location" 
-							read -e -p "Enter the path to libjack.dll: " -i "$HOME" LIBJACK_PATH
-							if [ -f $LIBJACK_PATH ]; then
-								sudo cp $LIBJACK_PATH $MXE/usr/i686-w64-mingw32.shared/bin/	
-							fi
-							if [ ! -e $MXE/usr/i686-w64-mingw32.shared/include/jack ]; then
-								ln -s ${CLONEPATH%/*}/jack2/common/jack $MXE/usr/i686-w64-mingw32.shared/include/jack
-							fi
-							break;;
-						[Nn]* ) break;;
-						* ) echo "Please answer yes or no.";;
-					esac
-				done
-			else
-				echo "libjack.dll was found at $MXE/usr/i686-w64-mingw32.shared/bin/"
-			fi
-			cmake ../.. -DCMAKE_TOOLCHAIN_FILE=$MXE/usr/i686-w64-mingw32.shared/share/cmake/mxe-conf.cmake
-			make
-			cd ..
-			export HYDROGEN
-			export HYDROGEN_BUILD
-			export MXE
-			./create_bundle.sh
-			if [ ! -d $HOME/Hydrogen ]; then
-				rm -rf $HOME/Hydrogen
-			fi
-			mv hydrogen_windows_32_bit $HOME/Hydrogen
-			#Check if PERMENANT_INSTALL is set to 1, and copy the files over
-			if [ "$PERMENANT_INSTALL" == "1" ]; then
-				sudo mv $MXE /opt/mxe
-			fi
+			build_hydrogen i686
 			;;
 		4)	#Experimental 64 Bit Compiling
-			echo "Coming Soon"
+			build_hydrogen x86_64
 			;;
 		5)	#Build Windows Installer
 			cd $HOME/Hydrogen
 			if [ ! -e sources ]; then
-				rm -rf $HYDROGEN/win32/windows_32_bit_build
+				rm -rf $HYDROGEN/windows
 				cp -r $HYDROGEN ./sources/
 			fi
 			if [ ! -e jack_installer ]; then
@@ -192,7 +221,7 @@ while :
 				wget http://www.gnu.org/licenses/gpl-3.0.txt
 			fi
 			if [ ! -e make_installer.nsi ]; then
-				cp $HYDROGEN/win32/make_installer.nsi ./
+				cp $HYDROGEN/windows/make_installer.nsi ./
 			fi
 			if [ ! -e "$HOME/Hydrogen/data/img/gray/h2-icon.ico" ]; then
 				convert $HOME/Hydrogen/data/img/gray/h2-icon.svg $HOME/Hydrogen/data/img/gray/h2-icon.ico
@@ -201,7 +230,7 @@ while :
 			;;
 		6)	#Clean up the files
 			echo "Now cleaning up the files. This process will move the built hydrogen into your home directory and delete the build files. If MXE was not permenantly installed, it will remove that too."
-			mv $HYDROGEN_BUILD/windows_32_bit_build $HOME/hydrogen_windows_32_bit_build
+			mv $HYDROGEN_BUILD $HOME/hydrogen_windows_32_bit_build
 			rm -rf $HYDROGEN
 			rm -rf $HOME/Hydrogen
 			if [ ! "$MXE_INSTALLED" == "1" ]; then
