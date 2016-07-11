07cae54f9e409618840150ed0f9688f87b267328
Sebastian Moors <mauser@smoors.de>
2016-07-11 22:10:25 +0200

    Merged pull request #291

--- src/gui/src/InstrumentEditor/InstrumentEditor.h
+++ src/gui/src/InstrumentEditor/InstrumentEditor.h
@@ -38,6 +38,7 @@ class Button;
 class ToggleButton;
 class ClickableLabel;
 class Rotary;
+class LCDCombo;
 class WaveDisplay;
 class LayerPreview;
 
@@ -47,7 +48,7 @@ class LayerPreview;
 ///
 class InstrumentEditor : public QWidget, public H2Core::Object, public EventListener
 {
-    H2_OBJECT
+	H2_OBJECT
 	Q_OBJECT
 
 	public:
@@ -84,6 +85,8 @@ class InstrumentEditor : public QWidget, public H2Core::Object, public EventList
 		void hihatMinRangeBtnClicked(Button *pRef);
 		void hihatMaxRangeBtnClicked(Button *pRef);
 
+		void pSampleSelectionChanged( QString );
+
 	private:
 		H2Core::Instrument *m_pInstrument;
 		int m_nSelectedLayer;
@@ -114,6 +117,7 @@ class InstrumentEditor : public QWidget, public H2Core::Object, public EventList
 		// Instrument gain
 		LCDDisplay *m_pInstrumentGainLCD;
 		Rotary *m_pInstrumentGain;
+
 		QCheckBox *m_pApplyVelocity;
 
 		// Instrument mute group
@@ -161,6 +165,9 @@ class InstrumentEditor : public QWidget, public H2Core::Object, public EventList
 
 		LCDDisplay *m_pLayerPitchCoarseLCD;
 		LCDDisplay *m_pLayerPitchFineLCD;
+
+		//LCDCombo *__pattern_size_combo;
+		LCDCombo *m_sampleSelectionAlg;
 
 		WaveDisplay *m_pWaveDisplay;
 
