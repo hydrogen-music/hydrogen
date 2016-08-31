35c9bb17dd6ae00e191783b83b872aaa564962f9
Olivier Humbert <trebmuh@users.noreply.github.com>
2016-08-31 13:12:13 +0200

    ML links update.
    In sync with the sourceforge possibilities.

--- src/gui/src/AboutDialog.cpp
+++ src/gui/src/AboutDialog.cpp
@@ -49,6 +49,8 @@ AboutDialog::AboutDialog(QWidget* parent)
 	about += tr( "<b>Project page</b><br>");
 	about += "http://sourceforge.net/projects/hydrogen<br><br>";
 	about += tr( "<b>Mailing lists</b><br>");
+	about += "http://lists.sourceforge.net/lists/listinfo/hydrogen-users<br>";
+	about += "https://lists.sourceforge.net/lists/listinfo/hydrogen-announce"<br>";
 	about += "http://lists.sourceforge.net/lists/listinfo/hydrogen-devel<br>";
 	aboutTxt->setText( about );
 
