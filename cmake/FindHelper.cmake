3df22a9cfa271c55e6153c1172cd562b8d4f1812
Jérémy Zurcher <jeremy@asynk.ch>
2017-08-01 01:53:18 +0200

    FindHelper.cmake : sanitize it

--- cmake/FindHelper.cmake
+++ cmake/FindHelper.cmake
@@ -2,53 +2,46 @@
 # - Try to find a library using pkg-config if available,
 #   than portable macros FIND_PATH and FIND_LIBRARY
 #
-# Once done this will define
+# The following variables will be set :
 #
-#  ${prefix}_FOUND          - system has the library
-#  ${prefix}_INCLUDE_DIR    - the library include directory
-#  ${prefix}_LIBRARIES      - Link these to library
-#  ${prefix}_DEFINITIONS    - Compiler switches required for using this libray
+#  ${prefix}_FOUND          - set to 1 or TRUE if found
+#  ${prefix}_INCLUDE_DIRS   - to be used in INCLUDE_DIRECTORIES(...)
+#  ${prefix}_LIBRARIES      - to be used in TARGET_LINK_LIBRARIES(...)
 #
-# Copyright (c) 2009, Jérémy Zurcher, <jeremy@asynk.ch>
+# Copyright (c) 2009-2017, Jérémy Zurcher, <jeremy@asynk.ch>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 macro(FIND_HELPER prefix pkg_name header lib)
-
-    if(${prefix}_INCLUDE_DIR AND ${prefix}_LIBRARIES)
+    if(${prefix}_INCLUDE_DIRS AND ${prefix}_LIBRARIES)
+        # use cached variables
         set(${prefix}_FIND_QUIETLY TRUE)
     else()
+        # try pkg-config
         if(NOT WIN32)
             FIND_PACKAGE(PkgConfig)
         endif()
         if(PKG_CONFIG_FOUND)
             pkg_check_modules(${prefix} ${pkg_name})
-            #MESSAGE(STATUS  " LDFLAGS       ${${prefix}_LDFLAGS}" )
-            #MESSAGE(STATUS  " CFLAGS        ${${prefix}_CFLAGS}" )
-            #MESSAGE(STATUS  " INCLUDE_DIRS  ${${prefix}_INCLUDE_DIRS}" )
-            set(${prefix}_DEFINITIONS ${prefix}_CFLAGS_OTHER )
         else()
             MESSAGE(STATUS "Checking for module '${pkg_name}'")
         endif()
-
-        #set(CMAKE_PREFIX_PATH ${${prefix}_PATH} )
-
-        find_path(${prefix}_INCLUDE_DIR
-            NAMES ${header}
-            HINTS ${${prefix}_INCLUDE_DIRS} ${${prefix}_INCLUDEDIR} ${${prefix}_INCLUDE_PATHS}
-            ENV ${prefix}_INCLUDE
-        )
-
-        find_library(${prefix}_LIBRARIES
-            NAMES ${lib}
-            HINTS ${${prefix}_LIBDIR} ${${prefix}_LIBRARY_DIRS} ${${prefix}_LIB_PATHS}
-            ENV ${prefix}_PATH
-        )
+        if(NOT ${prefix}_FOUND)
+            # try find_path and find_library
+            find_path(${prefix}_INCLUDE_DIRS
+                NAMES ${header}
+                ENV ${prefix}_INCLUDE
+            )
+            find_library(${prefix}_LIBRARIES
+                NAMES ${lib}
+                ENV ${prefix}_PATH
+            )
+            include(FindPackageHandleStandardArgs)
+            FIND_PACKAGE_HANDLE_STANDARD_ARGS(${prefix} DEFAULT_MSG ${prefix}_LIBRARIES ${prefix}_INCLUDE_DIRS)
+        endif()
     endif()
-
-    include(FindPackageHandleStandardArgs)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(${prefix} DEFAULT_MSG ${prefix}_LIBRARIES ${prefix}_INCLUDE_DIR)
-    mark_as_advanced(${prefix}_INCLUDE_DIR  ${prefix}_LIBRARIES)
-
+    #MESSAGE(STATUS  " FOUND     ${${prefix}_FOUND}" )
+    #MESSAGE(STATUS  " INCLUDE_DIRS  ${${prefix}_INCLUDE_DIRS}" )
+    #MESSAGE(STATUS  " LIBRARIES     ${${prefix}_LIBRARIES}" )
 endmacro()
