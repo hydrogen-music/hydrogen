9557d424d3b0be5447d5935a4c6499758a85a573
Jérémy Zurcher <jeremy@asynk.ch>
2017-09-04 15:35:30 +0200

    cmake/FindHelper.cmake final fix
    use pkg-config if available to set HINTS for find_path and find_library.
    the later sets FILEPATH= for ${prefix}_LIBRARIES which avoids bad
    linking on systems with multiple version of the same library.

--- cmake/FindHelper.cmake
+++ cmake/FindHelper.cmake
@@ -18,30 +18,28 @@ macro(FIND_HELPER prefix pkg_name header lib)
         # use cached variables
         set(${prefix}_FIND_QUIETLY TRUE)
     else()
-        # try pkg-config
+        # use pkg-config if available to set find_path and find_library hints
         if(NOT WIN32)
             FIND_PACKAGE(PkgConfig)
         endif()
         if(PKG_CONFIG_FOUND)
-            pkg_check_modules(${prefix} ${pkg_name})
+            pkg_check_modules(PC_${prefix} ${pkg_name})
         else()
             MESSAGE(STATUS "Checking for module '${pkg_name}'")
         endif()
-        if(NOT ${prefix}_FOUND)
-            # try find_path and find_library
-            find_path(${prefix}_INCLUDE_DIRS
-                NAMES ${header}
-                ENV ${prefix}_INCLUDE
+        # find_path
+        find_path(${prefix}_INCLUDE_DIRS
+            NAMES ${header}
+            HINTS ${PC_${prefix}_INCLUDEDIR} ${PC_${prefix}_INCLUDE_DIRS}
+            ENV ${prefix}_INCLUDE
             )
-            find_library(${prefix}_LIBRARIES
-                NAMES ${lib}
-                ENV ${prefix}_PATH
+        # find_library
+        find_library(${prefix}_LIBRARIES
+            NAMES ${lib}
+            HINTS ${PC_${prefix}_LIBDIR} ${PC_${prefix}_LIBRARY_DIRS}
+            ENV ${prefix}_PATH
             )
-            include(FindPackageHandleStandardArgs)
-            FIND_PACKAGE_HANDLE_STANDARD_ARGS(${prefix} DEFAULT_MSG ${prefix}_LIBRARIES ${prefix}_INCLUDE_DIRS)
-        endif()
+        include(FindPackageHandleStandardArgs)
+        FIND_PACKAGE_HANDLE_STANDARD_ARGS(${prefix} DEFAULT_MSG ${prefix}_LIBRARIES ${prefix}_INCLUDE_DIRS)
     endif()
-    #MESSAGE(STATUS  " FOUND     ${${prefix}_FOUND}" )
-    #MESSAGE(STATUS  " INCLUDE_DIRS  ${${prefix}_INCLUDE_DIRS}" )
-    #MESSAGE(STATUS  " LIBRARIES     ${${prefix}_LIBRARIES}" )
 endmacro()
