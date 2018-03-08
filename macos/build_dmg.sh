7038c6b5c41bd7a7a913bfd9d551a83e9240abed
Przemysław Sitek <przemyslaw.sitek@gmail.com>
2018-03-08 17:17:03 +0100

    Build DMG: Clean up

--- macos/build_dmg.sh
+++ macos/build_dmg.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 
-DMG_ROOT=`mktemp -d`
+
+# Default application bundle name
 SRC_APP="hydrogen.app"
+
+# Default dmg image name
 DMG_PATH="Hydrogen.dmg"
+
+# Option: enable verbosity
 VERBOSE=0
+
+# Option: open image after creating it
 OPEN=0
 
+
+# Print message if verbose mode is enabled
 function verbose {
 	if (( $VERBOSE )); then
 		echo $@
 	fi
 }
+
+
+# Show error message and exit (with cleanup)
 function error {
-	echo $@
+	echo $@ >&2
+	clean_up
 	exit 1
 }
+
+
+# Show usage info
 function usage {
 	echo "Usage: build_dmg.sh [-vho] [hydrogen.app] [hydrogen.dmg]"
 }
+
+
+# Print help message
 function show_help {
 	cat<<EOF
 Build Hydrogen .dmg image
@@ -30,6 +49,9 @@ Usage: build_dmg.sh [-vho] [hydrogen.app] [hydrogen.dmg]
 
 EOF
 }
+
+
+# Verify source app bundle
 function verify_app {
 	APP="$1"
 	if [ ! -d "$APP" ]; then
@@ -40,10 +62,20 @@ function verify_app {
 	fi
 }
 
+
+# Perform clean up; remove temporary directories
+function clean_up {
+	rm -rf "$DMG_ROOT"
+}
+
+
+
+
+# Parse options
+
 while getopts ":vho" opt; do
 	case $opt in
 		v)
-			echo "Enabling verbose mode"
 			VERBOSE=1
 			;;
 		h)
@@ -74,13 +106,16 @@ if (( $# > 2 )); then
 	exit 1
 fi
 
-verbose "Deploying $SRC_APP to $DMG_PATH"
+
+# Real work
 
 if [ -f "$DMG_PATH" ]; then
 	error "Output file exists"
 fi
 
 verify_app "$SRC_APP"
+
+DMG_ROOT=`mktemp -d`
 
 verbose "Copying application bundle"
 cp -r "$SRC_APP" "$DMG_ROOT/Hydrogen.app" || error "Can't copy $SRC_APP"
@@ -96,7 +131,7 @@ verbose "Creating dmg image"
 hdiutil create -srcfolder "$DMG_ROOT" -volname "Hydrogen" -fs HFS+ -format UDZO "$DMG_PATH" || error "Creating dmg image failed"
 
 verbose "Cleaning up"
-rm -rf "$DMG_ROOT"
+clean_up
 
 if (( $OPEN )); then
 	open "$DMG_PATH"
