45768fbd6acdd990e2164018f7e8375c25344e3b
Jérémy Zurcher <jeremy@asynk.ch>
2018-03-26 16:39:50 +0200

    build.sh : add missing H2FLAGS to z action

--- build.sh
+++ build.sh
@@ -98,7 +98,7 @@ function cmake_pkg() {
 
 function zoop() {
     cmake_make
-    LD_PRELOAD=$(find $BUILD_DIR -name 'libhydrogen-core*' | head -n 1) $BUILD_DIR/src/gui/hydrogen
+    LD_PRELOAD=$(find $BUILD_DIR -name 'libhydrogen-core*' | head -n 1) $BUILD_DIR/src/gui/hydrogen $H2FLAGS
 }
 
 if [ $# -eq 0 ]; then
