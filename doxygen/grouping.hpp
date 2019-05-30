7da2805394bd74532febc508671160eb015138b8
theGreatWhiteShark <thetruephil@googlemail.com>
2019-05-30 19:57:59 +0200

    Folder holding additional doc content All additional API and source documentation will go into the */doxygen*folder. I plan to write some pages describing the overall structure of H2, the interaction of GUI and core, the audio engine... It is probably a good idea to keep it separate from the actual source code.

---
+++ doxygen/grouping.hpp
@@ -0,0 +1,16 @@
+/** \defgroup h2core Core of Hydrogen
+*/
+
+/** \defgroup h2audiooEngine Parts of the source code belonging to the
+	audio engine and coping with the actual audio processing 
+* Ladida
+*/
+
+/** \page introduction A general overview
+ * \tableofcontents
+ * \section general
+ * \subsection gui Wraps \ref core
+ * \subsection core Blub
+ */
+
+/** \page advanced Some advanced pages.*/
