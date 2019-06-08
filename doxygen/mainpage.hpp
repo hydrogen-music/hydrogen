cd0f7a625065ffb9911113f5cf6f46613e4b6e05
theGreatWhiteShark <thetruephil@googlemail.com>
2019-06-08 19:33:22 +0200

    fixing typos

--- doxygen/mainpage.hpp
+++ doxygen/mainpage.hpp
@@ -8,9 +8,8 @@
  *  intuitive, pattern-based drum programming. Unlike other drum
  *  machines the C++ code base of Hydrogen and its slim Qt5-based GUI
  *  are tailored to be both memory-efficient and quite gentle in terms
- *  of CPU load. This makes it an excellent choice for especially
- *  older hardware or big setups consisting of numerous real-time
- *  programs. 
+ *  of CPU load. This makes it an excellent choice for older hardware
+ *  or big setups consisting of numerous real-time audio programs.
  *
  * \section structureCode Structure of the code
  * 
@@ -19,16 +18,16 @@
  * folder. The particular structure of this directory is meant to
  * imitate the overall design of the application. 
  *
- * For a more thorough introduction to both entities and their
- * interaction please see \ref generalDesign
+ * For a more thorough introduction please see \ref generalDesign.
  *
  * \subsection structureCore Core
- * It consists of a \b core, which is, among others, responsible for
- * generating all audio sounds, holding a representation of all
- * patterns, drumkits etc., for all interactions with, the operation
- * system and its audio driver and for interpreting and sending MIDI
- * signals. Within this folder you can find another separation into
- * the header files in
+ *
+ * Hydrogen consists of a \b core, which is responsible for, among
+ * others, generating all audio, holding representations of
+ * all patterns, drumkits etc., for all interactions with the
+ * operation system and its audio driver, and for interpreting and
+ * sending MIDI signals. Within this folder you can find another
+ * separation into the header files in
  * [src/core/include/hydrogen](https://github.com/hydrogen-music/hydrogen/tree/master/src/core/include/hydrogen)
  * and their implementation in
  * [src/core/src](https://github.com/hydrogen-music/hydrogen/tree/master/src/core/src). While
@@ -43,24 +42,25 @@
  *
  * \section structureDocumentation Structure of the documentation
  *
- * Since this project grew naturally and quite big already, the
+ * Since this project grew naturally (and quite big), the
  * encapsulation of related classes/functions into namespaces might
  * not be as good as it could be. 
  *
  * \subsection structureModules Modules
- * To still provide an overview about separate topic, the
+ * To still provide an overview about separate topics, the
  * documentation of the classes was grouped into logical units you can
  * access in [Modules](modules.html). \ref docCore contains all classes and
- * functions of the core while \ref docGUI encapsulate everything
- * related to the Qt5 GUI.
+ * functions of the core while \ref docGUI encapsulates everything
+ * related to the Qt5-based GUI.
  *
  * \subsection structureGD General Design
+ *
  * The code of Hydrogen grew quite complex in time and it can take a
- * lot of energy and time to learn how it works by the documentation
- * of the individual classes let alone its source code. To ease this
- * pain and to get new developers/contributors started more quickly,
- * we provide some articles giving a general and more accessible
- * overview about selected topics.
+ * lot of energy and time to learn how it works by using the
+ * documentation of the individual classes let alone their source
+ * code. To ease this pain and to get new developers/contributors
+ * started more quickly, we provide some articles giving a general and
+ * more accessible overview about selected topics.
  *
  * \subsection structureNM Namespace Members
  * Lists all members of the \b H2Core namespace encapsulating
@@ -83,20 +83,20 @@
  *
  * Get in touch with us via our [mailing
  * list](http://lists.sourceforge.net/mailman/listinfo/hydrogen-devel)
- * or by filing an issue on your
+ * or by filing an issue on our
  * [GitHub](https://github.com/hydrogen-music/hydrogen) page.
  *
  * \section contribute Want to contribute?
  *
- * Hydrogen is a community-driven project and depends on the
- * commitment of volunteers. So, even if you are not an experienced
- * C++ developer (yet), there are countless ways to contribute to this
- * project, like improving the documentation and manual, translating
- * the user manual, tweaking the design, a donation (for huge work of
- * our main contributor
+ * Hydrogen is a community-driven project and critically depends on
+ * the commitment of volunteers. So, even if you are not an
+ * experienced C++ developer (yet), there are countless ways to
+ * contribute to this project, like improving the documentation and
+ * manual, translating the user manual, tweaking the design, making a
+ * donation (for huge work of our main contributor
  * [mauser](https://www.paypal.com/donate/?token=VKmhes8It8cMNzqHmPXr1i7d2ZuFMpazjUG-68p1WlMWLCfgQmInVcBtBLtJJoKnDEgaPG&country.x=DE&locale.x=DE)
  * and hosting provided by
- * [wolke](https://www.paypal.com/donate/?token=T4bgCwNllD7bk7NaAeEETS5W3PyO6pYKoaha2q_yBWTWPIU1NfCOknv5iW-bxAzUqv7N_G&country.x=DE&locale.x=DE)
+ * [wolke](https://www.paypal.com/donate/?token=T4bgCwNllD7bk7NaAeEETS5W3PyO6pYKoaha2q_yBWTWPIU1NfCOknv5iW-bxAzUqv7N_G&country.x=DE&locale.x=DE))
  * etc. We are happy about all your efforts to contributed. The more
  * people we are, the more awesome features we can provide. ;)
  *
