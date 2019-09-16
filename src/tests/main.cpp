67a77716b1d78af80cfbc6a32a3b43a1d30e2232
Sebastian Moors <mauser@smoors.de>
2019-09-16 21:50:11 +0200

    tests/main.cpp: fix formatting

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -11,22 +11,22 @@
 
 void setupEnvironment(unsigned log_level)
 {
-    /* Logger */
-    H2Core::Logger* logger = H2Core::Logger::bootstrap( log_level );
+	/* Logger */
+	H2Core::Logger* logger = H2Core::Logger::bootstrap( log_level );
 	/* Test helper */
 	TestHelper::create_instance();
 	TestHelper* test_helper = TestHelper::get_instance();
-    /* Object */
-    H2Core::Object::bootstrap( logger, logger->should_log( H2Core::Logger::Debug ) );
-    /* Filesystem */
-    H2Core::Filesystem::bootstrap( logger, test_helper->data_dir() );
-    H2Core::Filesystem::info();
-
+	/* Object */
+	H2Core::Object::bootstrap( logger, logger->should_log( H2Core::Logger::Debug ) );
+	/* Filesystem */
+	H2Core::Filesystem::bootstrap( logger, test_helper->data_dir() );
+	H2Core::Filesystem::info();
+	
 	/* Use fake audio driver */
 	H2Core::Preferences::create_instance();
 	H2Core::Preferences* preferences = H2Core::Preferences::get_instance();
 	preferences->m_sAudioDriver = "Fake";
-
+	
 	H2Core::Hydrogen::create_instance();
 }
 
