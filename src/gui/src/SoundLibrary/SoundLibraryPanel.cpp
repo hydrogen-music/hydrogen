hydrogen-music/hydrogen

commit e9a1892dd08d0188f0cad7ad8a9d85e3e082bd2f

suppress Qt5 deprecation warnings

src/gui/src/SoundLibrary/SoundLibraryPanel.cpp
@@ -164,11 +164,11 @@ void SoundLibraryPanel::updateDrumkitList()
 
 	__system_drumkits_item = new QTreeWidgetItem( __sound_library_tree );
 	__system_drumkits_item->setText( 0, tr( "System drumkits" ) );
-	__sound_library_tree->setItemExpanded( __system_drumkits_item, true );
+	__system_drumkits_item->setExpanded( true );
 
 	__user_drumkits_item = new QTreeWidgetItem( __sound_library_tree );
 	__user_drumkits_item->setText( 0, tr( "User drumkits" ) );
-	__sound_library_tree->setItemExpanded( __user_drumkits_item, true );
+	__user_drumkits_item->setExpanded( true );
 
 	
 
@@ -192,7 +192,7 @@ void SoundLibraryPanel::updateDrumkitList()
 			QTreeWidgetItem* pDrumkitItem = new QTreeWidgetItem( __user_drumkits_item );
 			pDrumkitItem->setText( 0, pInfo->get_name() );
 			if ( QString(pInfo->get_name() ) == currentSL ){
-				pDrumkitItem->setBackgroundColor( 0, QColor( 50, 50, 50) );
+				pDrumkitItem->setBackground( 0, QColor( 50, 50, 50) );
 			}
 			InstrumentList *pInstrList = pInfo->get_instruments();
 			for ( uint nInstr = 0; nInstr < pInstrList->size(); ++nInstr ) {
@@ -214,7 +214,7 @@ void SoundLibraryPanel::updateDrumkitList()
 			QTreeWidgetItem* pDrumkitItem = new QTreeWidgetItem( __system_drumkits_item );
 			pDrumkitItem->setText( 0, pInfo->get_name() );
 			if ( QString( pInfo->get_name() ) == currentSL ){
-				pDrumkitItem->setBackgroundColor( 0, QColor( 50, 50, 50) );
+				pDrumkitItem->setBackground( 0, QColor( 50, 50, 50) );
 			}
 			InstrumentList *pInstrList = pInfo->get_instruments();
 			for ( uint nInstr = 0; nInstr < pInstrList->size(); ++nInstr ) {
@@ -232,7 +232,7 @@ void SoundLibraryPanel::updateDrumkitList()
 		__song_item = new QTreeWidgetItem( __sound_library_tree );
 		__song_item->setText( 0, tr( "Songs" ) );
 		__song_item->setToolTip( 0, tr("Double click to expand the list") );
-		__sound_library_tree->setItemExpanded( __song_item, __expand_songs_list );
+		__song_item->setExpanded( __expand_songs_list );
 		for (uint i = 0; i < songs.size(); i++) {
 			QTreeWidgetItem* pSongItem = new QTreeWidgetItem( __song_item );
 			QString song = songs[i];
@@ -249,7 +249,7 @@ void SoundLibraryPanel::updateDrumkitList()
 		__pattern_item = new QTreeWidgetItem( __sound_library_tree );
 		__pattern_item->setText( 0, tr( "Patterns" ) );
 		__pattern_item->setToolTip( 0, tr("Double click to expand the list") );
-		__sound_library_tree->setItemExpanded( __pattern_item, __expand_pattern_list );
+		__pattern_item->setExpanded( __expand_pattern_list );
 		
 		//this is the second step to push the mng.function
 		//SoundLibraryDatabase::create_instance();
@@ -449,7 +449,7 @@ void SoundLibraryPanel::on_DrumkitList_mouseMove( QMouseEvent *event)
 
 			pMimeData->setText( sText );
 			pDrag->setMimeData( pMimeData);
-			pDrag->start( Qt::CopyAction | Qt::MoveAction );
+			pDrag->exec( Qt::CopyAction | Qt::MoveAction );
 			return;
 		}
 
@@ -464,7 +464,7 @@ void SoundLibraryPanel::on_DrumkitList_mouseMove( QMouseEvent *event)
 		pMimeData->setText( sText );
 		pDrag->setMimeData( pMimeData);
 
-		pDrag->start( Qt::CopyAction | Qt::MoveAction );
+		pDrag->exec( Qt::CopyAction | Qt::MoveAction );
 	}
 }
 
@@ -572,7 +572,7 @@ void SoundLibraryPanel::on_drumkitLoadAction()
 
 	InstrumentEditorPanel::get_instance()->notifyOfDrumkitChange();
 
-	__sound_library_tree->currentItem()->setBackgroundColor ( 0, QColor( 50, 50, 50) );
+	__sound_library_tree->currentItem()->setBackground( 0, QColor( 50, 50, 50) );
 	QApplication::restoreOverrideCursor();
 
 }
@@ -606,14 +606,14 @@ void SoundLibraryPanel::change_background_color()
 
 	for (int i = 0; i < __system_drumkits_item->childCount() ; i++){
 		if ( ( __system_drumkits_item->child( i ) )->text( 0 ) == curlib ){
-			( __system_drumkits_item->child( i ) )->setBackgroundColor ( 0, QColor( 50, 50, 50)  );
+			( __system_drumkits_item->child( i ) )->setBackground( 0, QColor( 50, 50, 50)  );
 			break;
 		}
 	}
 
 	for (int i = 0; i < __user_drumkits_item->childCount() ; i++){
 		if ( ( __user_drumkits_item->child( i ))->text( 0 ) == curlib ){
-			( __user_drumkits_item->child( i ) )->setBackgroundColor ( 0, QColor( 50, 50, 50)  );
+			( __user_drumkits_item->child( i ) )->setBackground( 0, QColor( 50, 50, 50)  );
 			break;
 		}
 	}
@@ -805,8 +805,8 @@ void SoundLibraryPanel::on_patternDeleteAction()
 void SoundLibraryPanel::test_expandedItems()
 {
 	assert( __sound_library_tree );
-	__expand_songs_list = __sound_library_tree->isItemExpanded( __song_item );
-	__expand_pattern_list = __sound_library_tree->isItemExpanded( __pattern_item );
+	__expand_songs_list = __song_item->isExpanded();
+	__expand_pattern_list = __pattern_item->isExpanded();
 	Preferences::get_instance()->__expandSongItem = __expand_songs_list;
 	Preferences::get_instance()->__expandPatternItem = __expand_pattern_list;
 	//ERRORLOG( QString("songs %1 patterns %2").arg(__expand_songs_list).arg(__expand_pattern_list) );

src/gui/src/AudioFileBrowser/AudioFileBrowser.cpp
@@ -259,7 +259,7 @@ void AudioFileBrowser::browseTree( const QModelIndex& index )
 			m_pSamplerateLable->setText( tr( "Samplerate: %1" ).arg( pNewSample->get_sample_rate() ) );
 			float sec = ( float )( pNewSample->get_frames() / (float)pNewSample->get_sample_rate() );
 			QString qsec;
-			qsec.sprintf( "%2.2f", sec );
+			qsec.asprintf( "%2.2f", sec );
 			m_pLengthLable->setText( tr( "Sample length: " ) + qsec + tr( " s" ) );
 
 			delete pNewSample;

src/gui/src/HelpBrowser.cpp
@@ -78,7 +78,7 @@ SimpleHTMLBrowser::SimpleHTMLBrowser( QWidget *pParent, const QString& sDataPath
 		m_pBrowser->setHtml( stream.readAll() );
 	}
 
-	QRect rect( QApplication::desktop()->screenGeometry() );
+	QRect rect( QGuiApplication::screens().first()->geometry() );
 	move( rect.center() - this->rect().center() );
 }
 

src/gui/src/LadspaFXSelector.cpp
@@ -69,7 +69,7 @@ LadspaFXSelector::LadspaFXSelector(int nLadspaFX)
 	}
 	buildLadspaGroups();
 
-	m_pGroupsListView->setItemHidden( m_pGroupsListView->headerItem(), true );
+	m_pGroupsListView->headerItem()->setHidden( true );
 
 
 //	LadspaFXGroup* pFXGroup = LadspaFX::getLadspaFXGroup();

src/gui/src/PatternEditor/PatternEditorInstrumentList.cpp
@@ -730,7 +730,7 @@ void PatternEditorInstrumentList::mouseMoveEvent(QMouseEvent *event)
 	pMimeData->setText( sText );
 	pDrag->setMimeData( pMimeData);
 
-	pDrag->start( Qt::CopyAction | Qt::MoveAction );
+	pDrag->exec( Qt::CopyAction | Qt::MoveAction );
 
 	// propago l'evento
 	QWidget::mouseMoveEvent(event);

src/gui/src/PlaylistEditor/PlaylistDialog.cpp
@@ -249,9 +249,9 @@ PlaylistDialog::PlaylistDialog ( QWidget* pParent )
 		}
 
 		QTreeWidgetItem* m_pPlaylistItem = m_pPlaylistTree->topLevelItem ( aselected );
-		m_pPlaylistItem->setBackgroundColor ( 0, QColor( 50, 50, 50) );
-		m_pPlaylistItem->setBackgroundColor ( 1, QColor( 50, 50, 50) );
-		m_pPlaylistItem->setBackgroundColor ( 2, QColor( 50, 50, 50) );
+		m_pPlaylistItem->setBackground( 0, QColor( 50, 50, 50) );
+		m_pPlaylistItem->setBackground( 1, QColor( 50, 50, 50) );
+		m_pPlaylistItem->setBackground( 2, QColor( 50, 50, 50) );
 	}
 
 	timer = new QTimer( this );
@@ -919,9 +919,9 @@ void PlaylistDialog::updateActiveSongNumber()
 
 	QTreeWidgetItem* pPlaylistItem = m_pPlaylistTree->topLevelItem ( selected );
 	if ( pPlaylistItem != nullptr ){
-		pPlaylistItem->setBackgroundColor ( 0, QColor( 50, 50, 50) );
-		pPlaylistItem->setBackgroundColor ( 1, QColor( 50, 50, 50) );
-		pPlaylistItem->setBackgroundColor ( 2, QColor( 50, 50, 50) );
+		pPlaylistItem->setBackground( 0, QColor( 50, 50, 50) );
+		pPlaylistItem->setBackground( 1, QColor( 50, 50, 50) );
+		pPlaylistItem->setBackground( 2, QColor( 50, 50, 50) );
 	}
 }
 

src/gui/src/SongEditor/SongEditor.cpp
@@ -892,7 +892,7 @@ void SongEditor::drawPattern( int pos, int number, bool invertColour )
 	}
 
 	if ( bIsSelected ) {
-		patternColor = patternColor.dark( 130 );
+		patternColor = patternColor.darker( 130 );
 	}
 
 	int x = 10 + m_nGridWidth * pos;
@@ -1260,7 +1260,7 @@ void SongEditorPatternList::patternPopup_virtualPattern()
 		dialog->patternList->insertItem(0, newItem );
 
 		if (selectedPattern->get_virtual_patterns()->find(curPattern) != selectedPattern->get_virtual_patterns()->end()) {
-			dialog->patternList->setItemSelected(newItem, true);
+			newItem->setSelected( true );
 		}//if
 	}//for
 
@@ -1268,7 +1268,7 @@ void SongEditorPatternList::patternPopup_virtualPattern()
 		selectedPattern->virtual_patterns_clear();
 		for (unsigned int index = 0; index < listsize-1; ++index) {
 			QListWidgetItem *listItem = dialog->patternList->item(index);
-			if (dialog->patternList->isItemSelected(listItem) == true) {
+			if (listItem->isSelected() == true) {
 				if (patternNameMap.find(listItem->text()) != patternNameMap.end()) {
 					selectedPattern->virtual_patterns_add(patternNameMap[listItem->text()]);
 				}//if
@@ -1846,7 +1846,7 @@ void SongEditorPatternList::mouseMoveEvent(QMouseEvent *event)
 	pDrag->setMimeData( pMimeData);
 	//drag->setPixmap(iconPixmap);
 
-	pDrag->start( Qt::CopyAction | Qt::MoveAction );
+	pDrag->exec( Qt::CopyAction | Qt::MoveAction );
 
 	QWidget::mouseMoveEvent(event);
 }

src/gui/src/SoundLibrary/SoundLibraryImportDialog.cpp
@@ -374,16 +374,16 @@ void SoundLibraryImportDialog::updateSoundLibraryList()
 
 	m_pDrumkitsItem = new QTreeWidgetItem( m_pDrumkitTree );
 	m_pDrumkitsItem->setText( 0, tr( "Drumkits" ) );
-	m_pDrumkitTree->setItemExpanded( m_pDrumkitsItem, true );
+	m_pDrumkitsItem->setExpanded( true );
 
 
 	m_pSongItem = new QTreeWidgetItem( m_pDrumkitTree );
 	m_pSongItem->setText( 0, tr( "Songs" ) );
-	m_pDrumkitTree->setItemExpanded( m_pSongItem, true );
+	m_pSongItem->setExpanded( true );
 
 	m_pPatternItem = new QTreeWidgetItem( m_pDrumkitTree );
 	m_pPatternItem->setText( 0, tr( "Patterns" ) );
-	m_pDrumkitTree->setItemExpanded( m_pPatternItem, true );
+	m_pPatternItem->setExpanded( true );
 
 	for ( uint i = 0; i < m_soundLibraryList.size(); ++i ) {
 		QString sLibraryName = m_soundLibraryList[ i ].getName();

commit d232194999a4490f2e71a1df47be26a5f3926965

Fix Library Panel crash
Commit e9a1892dd0 caused a regression where having an empty song
list would result in a crash due to __song_item being uninitialised.

Previously QTreeWidget::isItemExpanded() would be called, which
would simply return false if nullptr would be passed to it.

src/gui/src/SoundLibrary/SoundLibraryPanel.cpp
@@ -805,8 +805,8 @@ void SoundLibraryPanel::on_patternDeleteAction()
 void SoundLibraryPanel::test_expandedItems()
 {
 	assert( __sound_library_tree );
-	__expand_songs_list = __song_item->isExpanded();
-	__expand_pattern_list = __pattern_item->isExpanded();
+	__expand_songs_list = ( __song_item ? __song_item->isExpanded() : false );
+	__expand_pattern_list = ( __pattern_item ? __pattern_item->isExpanded() : false );
 	Preferences::get_instance()->__expandSongItem = __expand_songs_list;
 	Preferences::get_instance()->__expandPatternItem = __expand_pattern_list;
 	//ERRORLOG( QString("songs %1 patterns %2").arg(__expand_songs_list).arg(__expand_pattern_list) );