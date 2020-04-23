7d6d45c703f6f2323a7e4595d90f724533176a73
Jérémy Zurcher <jeremy@asynk.ch>
2020-04-23 15:39:22 +0200

    test : add loading and check of an old dk from 2008

--- src/tests/xml_test.h
+++ src/tests/xml_test.h
@@ -6,6 +6,7 @@
 class XmlTest : public CppUnit::TestCase {
 	CPPUNIT_TEST_SUITE(XmlTest);
 	CPPUNIT_TEST(testDrumkit);
+	CPPUNIT_TEST(testDrumkit2008);
 	CPPUNIT_TEST(testDrumkit_UpgradeInvalidADSRValues);
 	CPPUNIT_TEST(testPattern);
 	CPPUNIT_TEST_SUITE_END();
@@ -13,8 +14,8 @@ class XmlTest : public CppUnit::TestCase {
 	public:
 		void testDrumkit();
 		void testDrumkit_UpgradeInvalidADSRValues();
+		void testDrumkit2008();
 		void testPattern();
-	
 };
 
 
