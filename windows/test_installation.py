a7f44df2e3b56f937c251b7b5764800ce983f4fe
Przemysław Sitek <przemyslaw.sitek@gmail.com>
2020-05-02 11:26:10 +0200

    Windows CI Pipeline: Make installer

---
+++ windows/test_installation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import pytest
+import os
+import os.path
+
+FILES = [
+    "hydrogen.exe",
+    "libhydrogen-core-1.0.0.dll",
+    "data/hydrogen.default.conf",
+    "data/i18n/hydrogen_hu_HU.qm"
+]
+
+ROOT = os.getenv("INSTDIR", default=r"C:\Program Files\Hydrogen")
+
+
+@pytest.mark.parametrize("file_name", FILES)
+def test_installed_file(file_name):
+    full_path = os.path.join(ROOT, file_name)
+    assert os.path.exists(full_path)
