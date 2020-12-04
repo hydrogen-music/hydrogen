04f824fa9ec43327f470c6fd649cde984a3c9cb3
phil <thetruephil@googlemail.com>
2020-12-04 22:26:55 +0100

    include MacOS no access file path

--- src/tests/FilesystemTest.cpp
+++ src/tests/FilesystemTest.cpp
@@ -6,9 +6,13 @@
 using namespace H2Core;
 
 void FilesystemTest::setUp() {
-#if !defined(Q_OS_MACX) || !defined(WIN32) 
+#if !defined(WIN32) 
 	m_sNotExistingPath = QDir::homePath().append( "aFunnyNameYouWouldNotExpectInYourHomeFolder.h2song" );
+#ifdef Q_OS_MACX
+	m_sNoAccessPath = "/etc/master.passwd";
+#else
 	m_sNoAccessPath = "/etc/shadow";
+#endif
 	m_sReadOnlyPath = "/etc/profile";
 	m_sFullAccessPath = Filesystem::usr_data_path().append( "test.h2song"  );
 	m_sTmpPath = Filesystem::tmp_file_path( "test.h2song" );
@@ -16,13 +20,13 @@ void FilesystemTest::setUp() {
 }
 
 void FilesystemTest::tearDown() {
-#if !defined(Q_OS_MACX) || !defined(WIN32) 
+#ifndef WIN32
 	CPPUNIT_ASSERT( Filesystem::rm( m_sTmpPath, false ) );
 #endif
 }
 
 void FilesystemTest::testPermissions(){
-#if !defined(Q_OS_MACX) || !defined(WIN32) 
+#ifndef WIN32
 	CPPUNIT_ASSERT( ! Filesystem::file_exists( m_sNotExistingPath, true ) );
 	CPPUNIT_ASSERT( Filesystem::file_exists( m_sNoAccessPath, true ) );
 	CPPUNIT_ASSERT( ! Filesystem::file_readable( m_sNoAccessPath, true ) );
