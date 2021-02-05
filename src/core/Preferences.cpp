91312e119e9470b25b63ba1c670b2b23c8d84b9e
theGreatWhiteShark <thetruephil@googlemail.com>
2021-02-05 14:43:45 +0100

    resilient driver loading in Preferences
    a couple of weeks ago I updated the options in the PreferencesDialog to ensure consistency in style and spelling. This, among others, lead to capitalizing all acronymns.
    
    When updating Hydrogen from an older version the GUI would not recognized the last audio driver if "Jack", "Alsa" or "Oss" nor the last MIDI driver if "JackMidi" or "CoreMidi" were used. This is itself not a major problem since a single selection of the driver in the PreferencesDialog will solve this conflict. But, nevertheless, this is an avoidable inconvenience.
    
    I added a couple of lines to support the out-dated options too. They are intended to remain only temporary - e.g. during the transition 1.0 => 1.1 - and can be dropped in the release thereafter since most of the user might have happened to open Hydrogen between updates.

--- src/core/Preferences.cpp
+++ src/core/Preferences.cpp
@@ -399,6 +399,17 @@ void Preferences::loadPreferences( bool bGlobal )
 				recreate = true;
 			} else {
 				m_sAudioDriver = LocalFileMng::readXmlString( audioEngineNode, "audio_driver", m_sAudioDriver );
+				// Ensure compatibility will older versions of the
+				// files after capitalization in the GUI
+				// (2021-02-05). This can be dropped in releases >=
+				// 1.2
+				if ( m_sAudioDriver == "Jack" ) {
+					m_sAudioDriver = "JACK";
+				} else if ( m_sAudioDriver == "Oss" ) {
+					m_sAudioDriver = "OSS";
+				} else if ( m_sAudioDriver == "Alsa" ) {
+					m_sAudioDriver = "ALSA";
+				}
 				m_bUseMetronome = LocalFileMng::readXmlBool( audioEngineNode, "use_metronome", m_bUseMetronome );
 				m_fMetronomeVolume = LocalFileMng::readXmlFloat( audioEngineNode, "metronome_volume", 0.5f );
 				m_nMaxNotes = LocalFileMng::readXmlInt( audioEngineNode, "maxNotes", m_nMaxNotes );
@@ -485,6 +496,15 @@ void Preferences::loadPreferences( bool bGlobal )
 					recreate = true;
 				} else {
 					m_sMidiDriver = LocalFileMng::readXmlString( midiDriverNode, "driverName", "ALSA" );
+					// Ensure compatibility will older versions of the
+					// files after capitalization in the GUI
+					// (2021-02-05). This can be dropped in releases
+					// >= 1.2
+					if ( m_sAudioDriver == "JackMidi" ) {
+						m_sAudioDriver = "JACK-MIDI";
+					} else if ( m_sAudioDriver == "CoreMidi" ) {
+						m_sAudioDriver = "CoreMIDI";
+					}
 					m_sMidiPortName = LocalFileMng::readXmlString( midiDriverNode, "port_name", "None" );
 					m_sMidiOutputPortName = LocalFileMng::readXmlString( midiDriverNode, "output_port_name", "None" );
 					m_nMidiChannelFilter = LocalFileMng::readXmlInt( midiDriverNode, "channel_filter", -1 );
