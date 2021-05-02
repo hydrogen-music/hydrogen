993a6ed4f1eaf57986f2589f404c513228c11217
Colin McEwan <colin.mcewan@gmail.com>
2021-05-02 14:37:51 +0100

    Fix #1254 (mouse cursor gets stuck after cancelled keyboard gesture)

--- src/gui/src/SongEditor/SongEditor.cpp
+++ src/gui/src/SongEditor/SongEditor.cpp
@@ -607,7 +607,7 @@ void SongEditor::updateModifiers( QInputEvent *ev )
 		}
 	}
 
-	if ( m_selection.isMoving() ) {
+	if ( m_selection.isMouseGesture() && m_selection.isMoving() ) {
 		// If a selection is currently being moved, change the cursor
 		// appropriately. Selection will change it back after the move
 		// is complete (or abandoned)
