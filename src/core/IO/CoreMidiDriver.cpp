897b4f44f43dad6b0fba6716b9830cf0ed184835
phil <thetruephil@googlemail.com>
2021-06-07 14:19:46 +0200

    replace postal address of FSF by URL

--- src/core/IO/CoreMidiDriver.cpp
+++ src/core/IO/CoreMidiDriver.cpp
@@ -19,8 +19,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with this program. If not, see https://www.gnu.org/licenses
  *
  * Code cleanup (20060222 Jonathan Dempsey)
  * More cleaning (20060511 Jonathan Dempsey)
