ccd2e97ccf62613c91ad995e6f5df8bd0e612e2f
Charbel Jacquin <charbel.jacquin@gmail.com>
2021-06-23 15:51:35 +0200

    rework example

--- src/shiboken_bindings/examples/test_DetailWaveDisplay.py
+++ src/shiboken_bindings/examples/test_DetailWaveDisplay.py
@@ -1,18 +1,9 @@
 from PySide2.QtCore import QObject
 from PySide2.QtWidgets import QFrame, QApplication
-from h2core import H2Core as h2core
-Logger = h2core.Logger
-logger = Logger.bootstrap(
-    Logger.Debug | Logger.Error | Logger.Warning | Logger.Info
-)
-h2core.Object.bootstrap(logger, True)
-h2core.Filesystem.bootstrap(logger, "/usr/local/share/hydrogen/data/")
-prefs = h2core.Preferences.create_instance()
-hydrogen = h2core.Hydrogen.create_instance()
-song = h2core.Song("foo", "bar", 120, 1)
-print(song)
+
 import h2gui
-interactive = True 
+
+interactive = True
 app = QApplication.instance()
 if not app:
     app = QApplication()
@@ -22,17 +13,18 @@ fr = QFrame()
 
 widget = h2gui.TargetWaveDisplay(fr)
 
-class Spy(QObject):
 
+class Spy(QObject):
     def eventFilter(self, obj, event):
         print("e", obj, event)
         return QObject.eventFilter(self, obj, event)
-    
+
     def envelopeEdited(self, mode):
         print("EE", mode)
 
     def doneEnvelopeEditing(self, mode):
         print("DE", mode)
+
 
 spy = Spy()
 
@@ -44,5 +36,3 @@ fr.show()
 
 if not interactive:
     app.exec_()
-
-
