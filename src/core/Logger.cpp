fada1a7611ff65f9fe5c1a4eeb64df28974f0ef7
Raphael Graf <r@undefined.ch>
2021-12-25 14:53:37 +0100

    Lock mutex before pthread_cond_wait

--- src/core/Logger.cpp
+++ src/core/Logger.cpp
@@ -65,6 +65,7 @@ void* loggerThread_func( void* param ) {
 	Logger::queue_t::iterator it, last;
 
 	while ( logger->__running ) {
+		pthread_mutex_lock( &logger->__mutex );
 		pthread_cond_wait( &logger->__messages_available, &logger->__mutex );
 		pthread_mutex_unlock( &logger->__mutex );
 		if( !queue->empty() ) {
