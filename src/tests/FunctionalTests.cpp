ec8bcdcec618052074286a75d81bdd15804f9491
Colin McEwan <colin.mcewan@gmail.com>
2022-01-23 00:03:29 +0000

    Avoid spurious sleeps in audio export
    Previously while writing the audio output, the test would sleep for
    0.1s for every event emitted while processing. Since there are many
    more events than just progress events, this led to many sleeps.
    Changing the loop to only sleep on an empty event queue removes
    this. And so shaves a few seconds off Appveyor builds too.

--- src/tests/FunctionalTests.cpp
+++ src/tests/FunctionalTests.cpp
@@ -86,7 +86,7 @@ void exportSong( const QString &songFile, const QString &fileName )
 		if (event.type == EVENT_PROGRESS && event.value == 100) {
 			done = true;
 		}
-		else {
+		else if ( event.type == EVENT_NONE ) {
 			usleep(100 * 1000);
 		}
 	}
