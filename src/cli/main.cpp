fabe18b43a0c0ec467428363c2825536c5df3050
phil <thetruephil@googlemail.com>
2022-03-31 09:32:39 +0200

    CLI: delete playlist
    The playlist was still alive after exporting a song

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -510,7 +510,7 @@ int main(int argc, char *argv[])
 		}
 
 		pSong = nullptr;
-		delete pPlaylist;
+		delete Playlist::get_instance();
 
 		preferences->savePreferences();
 		delete pHydrogen;
@@ -523,10 +523,8 @@ int main(int argc, char *argv[])
 		___INFOLOG( "Quitting..." );
 		delete Logger::get_instance();
 
-		int nObj = Base::objects_count();
-		if (nObj != 0) {
-			std::cerr << "\n\n\n " << nObj << " alive objects\n\n" << std::endl << std::endl;
-			Base::write_objects_map_to_cerr();
+		if (H2Core::Base::count_active()) {
+			H2Core::Base::write_objects_map_to_cerr();
 		}
 	}
 	catch ( const H2Exception& ex ) {
