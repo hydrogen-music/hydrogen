becf5701a1ed1cdf4ff1a51db9a7db9479e5516c
theGreatWhiteShark <princess.trudildis@posteo.de>
2022-07-20 22:34:45 +0200

    LCDDisplay: allow active usage
    previously this widget was only available disabled. Now, it can be used enabled too in order to make use of it in e.g. custom dialogs

--- src/gui/src/Widgets/LCDDisplay.h
+++ src/gui/src/Widgets/LCDDisplay.h
@@ -42,10 +42,13 @@ class LCDDisplay : public QLineEdit, public H2Core::Object<LCDDisplay>
 	Q_OBJECT
 
 public:
-	LCDDisplay( QWidget* pParent, QSize size, bool bFixedFont = false );
+	LCDDisplay( QWidget* pParent, QSize size = QSize( 0, 0 ), bool bFixedFont = false, bool bIsActive = true );
 	~LCDDisplay();
 
 	void setUseRedFont( bool bUseRedFont );
+	
+	bool getIsActive() const;
+	void setIsActive( bool bIsActive );
 
 public slots:
 	void onPreferencesChanged( H2Core::Preferences::Changes changes );
@@ -57,10 +60,14 @@ private:
 
 	bool m_bFixedFont;
 	bool m_bUseRedFont;
+	bool m_bIsActive;
 
 	std::vector<int> m_fontPointSizes;
 	
 	virtual void paintEvent( QPaintEvent *ev ) override;
 };
+inline bool LCDDisplay::getIsActive() const {
+	return m_bIsActive;
+}
 
 #endif
