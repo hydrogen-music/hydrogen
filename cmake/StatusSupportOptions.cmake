19c69c8d2d2816cb564eaa39c89e077828fc6ec1
theGreatWhiteShark <princess.trudildis@posteo.de>
2023-06-11 12:45:49 +0200

    cmake: print library versions
    the code for creating summary of the status of each package in cmake did contain the `<IBRARY>_VERSION` variable. However, this one is nowhere set in the code and probably is an artifact from former versions of the cmake config script. If available - which is true for most packages -, we use the `pkg-config` support of cmake to find the path and version of a library. The later, however, is written to `PC_<LIBRARY>_VERSION`. All of those are now written to the summary printed during the build process.

--- cmake/StatusSupportOptions.cmake
+++ cmake/StatusSupportOptions.cmake
@@ -25,17 +25,31 @@ macro(COMPUTE_PKGS_FLAGS prefix)
         SET(${prefix}_LIBRARIES "" )
         SET(${prefix}_INCLUDE_DIRS "" )
     endif()
+	
+	set(VERSION_STRING "")
+	if(NOT "${${prefix}_VERSION}" STREQUAL "")
+		# version discovered using project-specific .cmake file and
+		# (cmake command find_package).
+		set(VERSION_STRING "[ ${${prefix}_VERSION} ] ")
+	endif()
+	if(NOT "${PC_${prefix}_VERSION}" STREQUAL "")
+		# version discovered using project-specific .pc file and
+		# `pkg-config`. This is the default way for determining
+		# versions of most packages we use.
+		set(VERSION_STRING "[ ${PC_${prefix}_VERSION} ] ")
+	endif()
+	
     if(${prefix}_NOT_NEEDED)
         if(${prefix}_FOUND)
-            SET(${prefix}_STATUS "${_escape}[0;33m| available\${_escape}[0m but not needed ${${prefix}_VERSION} ( ${${prefix}_LIBRARIES} )")
+            SET(${prefix}_STATUS "${_escape}[0;33m| available\${_escape}[0m but not needed ${VERSION_STRING} ( ${${prefix}_LIBRARIES} )")
         else()
             SET(${prefix}_STATUS "${_escape}[1;33m| not found\${_escape}[0m but not needed" )
         endif()
     elseif(${prefix}_FOUND)
         if(NOT WANT_${prefix})
-            SET(${prefix}_STATUS "${_escape}[0;32m? available${_escape}[0m but not desired ${${prefix}_VERSION} ( ${${prefix}_LIBRARIES} )")
+            SET(${prefix}_STATUS "${_escape}[0;32m? available${_escape}[0m but not desired ${VERSION_STRING} ( ${${prefix}_LIBRARIES} )")
         else()
-            SET(${prefix}_STATUS "${_escape}[1;32m+ used${_escape}[0m ${${prefix}_VERSION} ( ${${prefix}_LIBRARIES} )")
+            SET(${prefix}_STATUS "${_escape}[1;32m+ used${_escape}[0m ${VERSION_STRING}( ${${prefix}_LIBRARIES} )")
             SET(H2CORE_HAVE_${prefix} TRUE)
         endif()
     else()
