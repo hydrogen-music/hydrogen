7f54bb60efc4e1228d50a1ce8cb2653f40fe5b0b
theGreatWhiteShark <princess.trudildis@posteo.de>
2023-10-10 20:39:48 +0200

    tests: better coverage in export song test
    try
    - different sample rates and sample depths
    - different ADSR settings
    - High pass and resonance filtering
    - different layer selection algorithms
    - stop notes
    - notes with user-defined length
    - auto stop note
    - non-default pitch settings

--- src/tests/FunctionalTests.cpp
+++ src/tests/FunctionalTests.cpp
@@ -154,13 +154,39 @@ public:
 	void testExportAudio()
 	{
 	___INFOLOG( "" );
-		auto songFile = H2TEST_FILE("functional/test.h2song");
-		auto outFile = Filesystem::tmp_file_path("test.wav");
-		auto refFile = H2TEST_FILE("functional/test.ref.flac");
+		const auto sSongFile = H2TEST_FILE("functional/test_adsr.h2song");
 
-		TestHelper::exportSong( songFile, outFile );
-		H2TEST_ASSERT_AUDIO_FILES_EQUAL( refFile, outFile );
-		Filesystem::rm( outFile );
+		// Test exporting using a couple of different sample rate (to test both
+		// resampling and no resampling) and sample depths.
+		struct Setup {
+			QString sTempFile;
+			QString sReferenceFile;
+			int nSampleRate;
+			int nSampleDepth;
+		};
+
+		std::vector<Setup> setups;
+		setups.push_back( { "test-44100-16.wav",
+				"functional/test-44100-16.ref.flac", 44100, 16} );
+		setups.push_back( { "test-48000-16.wav",
+				"functional/test-48000-16.ref.flac", 48000, 16} );
+		setups.push_back( { "test-48000-32.wav",
+				"functional/test-48000-32.ref.flac", 48000, 32} );
+		setups.push_back( { "test-44100-8.wav",
+				"functional/test-44100-8.ref.flac", 44100, 8} );
+
+		for ( int ii = 0; ii < setups.size(); ++ii ) {
+			___INFOLOG( QString( "Testing sample rate: [%1] and depth: [%2]" )
+						.arg( setups[ii].nSampleRate )
+						.arg( setups[ii].nSampleDepth ) );
+			const auto sOutFile = Filesystem::tmp_file_path( setups[ii].sTempFile );
+			const auto sRefFile = H2TEST_FILE( setups[ii].sReferenceFile );
+
+			TestHelper::exportSong( sSongFile, sOutFile, setups[ii].nSampleRate,
+									setups[ii].nSampleDepth );
+			H2TEST_ASSERT_AUDIO_FILES_EQUAL( sRefFile, sOutFile );
+			Filesystem::rm( sOutFile );
+		}
 	___INFOLOG( "passed" );
 	}
 
