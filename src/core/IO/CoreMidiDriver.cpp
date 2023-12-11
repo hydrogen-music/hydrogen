65c233c2fdfb8bbaf7a7fe998a277196194b2ca1
theGreatWhiteShark <princess.trudildis@posteo.de>
2023-12-11 18:24:02 +0100

    CoreMidiDriver: add missing includes

--- src/core/IO/CoreMidiDriver.cpp
+++ src/core/IO/CoreMidiDriver.cpp
@@ -29,6 +29,8 @@
 
 #include <core/Hydrogen.h>
 #include <core/Basics/Note.h>
+#include <core/Basics/Song.h>
+#include <core/Basics/Drumkit.h>
 #include <core/Basics/Instrument.h>
 #include <core/Basics/InstrumentList.h>
 #include <core/Preferences/Preferences.h>
