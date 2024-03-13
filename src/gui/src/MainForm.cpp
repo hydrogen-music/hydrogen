9743a8e1335091a70b99ef722249446860def361
theGreatWhiteShark <princess.trudildis@posteo.de>
2024-03-13 19:33:45 +0100

    Hydrogen: drop pan in addRealtimeNote
    the pan of a freshly added note seem to have been customizable when for MIDI NOTE_ON events in the pre `git` era of Hydrogen. But by no, only 0 is provided and the variable can be dropped

--- src/gui/src/MainForm.cpp
+++ src/gui/src/MainForm.cpp
@@ -1958,7 +1958,7 @@ bool MainForm::eventFilter( QObject *o, QEvent *e )
 
 				float velocity = 0.8;
 
-				pHydrogen->addRealtimeNote( row, velocity, 0.f, false, row + 36 );
+				pHydrogen->addRealtimeNote( row, velocity, false, row + 36 );
 
 				return true; // eat event
 			}
