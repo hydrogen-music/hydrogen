d22b31191baee883b5a606790f79a21954a084d4
theGreatWhiteShark <princess.trudildis@posteo.de>
2024-04-09 23:06:27 +0200

    tests: add unit test for UTF-8 DK import/export

---
+++ src/tests/DrumkitExportTest.h
@@ -0,0 +1,49 @@
+/*
+ * Hydrogen
+ * Copyright(c) 2002-2008 by Alex >Comix< Cominu [comix@users.sourceforge.net]
+ * Copyright(c) 2008-2024 The hydrogen development team [hydrogen-devel@lists.sourceforge.net]
+ *
+ * http://www.hydrogen-music.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses
+ *
+ */
+
+#ifndef DRUMKIT_EXPORT_TEST_H
+#define DRUMKIT_EXPORT_TEST_H
+
+#include <QString>
+#include <cppunit/extensions/HelperMacros.h>
+
+class DrumkitExportTest : public CppUnit::TestCase {
+
+// Otherwise either export won't work or a command line zip is used, which is
+// bound to produce slightly different results.
+#ifdef H2CORE_HAVE_LIBARCHIVE
+	CPPUNIT_TEST_SUITE( DrumkitExportTest );
+	CPPUNIT_TEST( testDrumkitExportAndImport );
+	CPPUNIT_TEST_SUITE_END();
+#endif
+
+	private:
+		QString m_sTestKitName;
+
+	public:
+	virtual void setUp();
+	virtual void tearDown();
+	
+	void testDrumkitExportAndImport();
+};
+
+#endif
