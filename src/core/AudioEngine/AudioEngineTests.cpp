c2be5b8ff92512d5544dcc92d5c094ae17b169db
theGreatWhiteShark <princess.trudildis@posteo.de>
2024-05-23 21:11:21 +0200

    AETests: fix AE locking in JACK tests

--- src/core/AudioEngine/AudioEngineTests.cpp
+++ src/core/AudioEngine/AudioEngineTests.cpp
@@ -2148,7 +2148,9 @@ void AudioEngineTests::testTransportRelocationJack() {
 					pAE->unlock();
 				}
 				else {
+					pAE->lock( RIGHT_HERE );
 					pDriver->locateTransport( nFrame );
+					pAE->unlock();
 				}
 			}
 
@@ -2201,15 +2203,11 @@ void AudioEngineTests::testTransportRelocationJack() {
 			nNewFrame = 2174246;
 		}
 
-		if ( m_referenceTimebase == JackAudioDriver::Timebase::Master ) {
-			pAE->lock( RIGHT_HERE );
-			pAE->locateToFrame( nNewFrame );
-			pAE->unlock();
-		}
-		else {
-			pDriver->locateTransport( nNewFrame );
-			waitForRelocation( -1, nNewFrame );
-		}
+		pAE->lock( RIGHT_HERE );
+		pDriver->locateTransport( nNewFrame );
+		pAE->unlock();
+
+		waitForRelocation( -1, nNewFrame );
 
 		if ( nNewFrame != pTransportPos->getFrame() -
 			 pTransportPos->getFrameOffsetTempo() ) {
