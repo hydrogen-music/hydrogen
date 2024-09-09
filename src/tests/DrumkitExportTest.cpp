825658b4e07b16a7554b05f5040d4877c9738fca
theGreatWhiteShark <princess.trudildis@posteo.de>
2024-09-09 21:45:02 +0200

    tests/DrumkitExportTest: add unit test for formats
    the corresponding kit contains samples in all supported file formats. All of them need to be able to be exported and imported on all supported platforms.

--- src/tests/DrumkitExportTest.cpp
+++ src/tests/DrumkitExportTest.cpp
@@ -41,12 +41,16 @@ using namespace H2Core;
 void DrumkitExportTest::setUp() {
 	// Ensure the test kit is not installed on the system.
 	m_sTestKitName = "testKit";
+	m_sTestKitNameSampleFormats = "sampleFormatKit";
 	m_sTestKitNameUtf8 = "ếИ£TestKit越";
 
 	// We do not check return value as the folder should not exist in the first
 	// place.
 	if ( Filesystem::dir_exists( Filesystem::drumkit_usr_path( m_sTestKitName ) ) ) {
 		Filesystem::rm( Filesystem::drumkit_usr_path( m_sTestKitName ), true, true );
+	}
+	if ( Filesystem::dir_exists( Filesystem::drumkit_usr_path( m_sTestKitNameSampleFormats ) ) ) {
+		Filesystem::rm( Filesystem::drumkit_usr_path( m_sTestKitNameSampleFormats ), true, true );
 	}
 	if ( Filesystem::dir_exists( Filesystem::drumkit_usr_path( m_sTestKitNameUtf8 ) ) ) {
 		Filesystem::rm( Filesystem::drumkit_usr_path( m_sTestKitNameUtf8 ), true, true );
@@ -60,6 +64,9 @@ void DrumkitExportTest::tearDown() {
 	// Remove the test kit from the system.
 	if ( Filesystem::dir_exists( Filesystem::drumkit_usr_path( m_sTestKitName ) ) ) {
 		Filesystem::rm( Filesystem::drumkit_usr_path( m_sTestKitName ), true, true );
+	}
+	if ( Filesystem::dir_exists( Filesystem::drumkit_usr_path( m_sTestKitNameSampleFormats ) ) ) {
+		Filesystem::rm( Filesystem::drumkit_usr_path( m_sTestKitNameSampleFormats ), true, true );
 	}
 	if ( Filesystem::dir_exists( Filesystem::drumkit_usr_path( m_sTestKitNameUtf8 ) ) ) {
 		Filesystem::rm( Filesystem::drumkit_usr_path( m_sTestKitNameUtf8 ), true, true );
@@ -105,6 +112,57 @@ void DrumkitExportTest::testDrumkitExportAndImport() {
 	// just exported.
 	const QString sExportPath = QString( "%1%2%3" )
 		.arg( Filesystem::tmp_dir() ).arg( m_sTestKitName )
+		.arg( Filesystem::drumkit_ext );
+	QTemporaryDir exportValidation( H2Core::Filesystem::tmp_dir() + "-XXXXXX" );
+	exportValidation.setAutoRemove( false );
+	CPPUNIT_ASSERT( pCoreActionController->extractDrumkit(
+						sExportPath, exportValidation.path() ) );
+
+	H2TEST_ASSERT_DIRS_EQUAL( exportValidation.path(), sExtractedKit );
+
+	// Cleanup
+	H2Core::Filesystem::rm( exportValidation.path(), true, true );
+
+	___INFOLOG( "passed" );
+}
+
+void DrumkitExportTest::testDrumkitExportAndImportSampleFormats() {
+	___INFOLOG( "" );
+
+	auto pHydrogen = Hydrogen::get_instance();
+	auto pCoreActionController = pHydrogen->getCoreActionController();
+
+	const QString sTestKitPath =
+		H2TEST_FILE( QString( "drumkits/%1%2" )
+					 .arg( m_sTestKitNameSampleFormats )
+					 .arg( Filesystem::drumkit_ext ) );
+
+	// Check validity of test kit
+	CPPUNIT_ASSERT( pCoreActionController->validateDrumkit(
+						sTestKitPath, false ) );
+
+	// Import test kit into Hydrogen.
+	CPPUNIT_ASSERT( pCoreActionController->extractDrumkit( sTestKitPath ) );
+
+	// Check whether import worked, the UTF-8 path and name was read properly,
+	// and all samples are present.
+	const auto pDB = pHydrogen->getSoundLibraryDatabase();
+	const QString sExtractedKit =
+		Filesystem::drumkit_usr_path( m_sTestKitNameSampleFormats );
+	const auto pDrumkit = pDB->getDrumkit( sExtractedKit, true );
+	CPPUNIT_ASSERT( pDrumkit != nullptr );
+	CPPUNIT_ASSERT( pDrumkit->get_name() == m_sTestKitNameSampleFormats );
+	for ( const auto& ppInstrument : *pDrumkit->get_instruments() ) {
+		CPPUNIT_ASSERT( ! ppInstrument->has_missing_samples() );
+	}
+
+	// Load the kit and export it.
+	CPPUNIT_ASSERT( pDrumkit->exportTo( Filesystem::tmp_dir() ) );
+
+	// Bitwise comparison of the (!extracted!) original drumkit and the one we
+	// just exported.
+	const QString sExportPath = QString( "%1%2%3" )
+		.arg( Filesystem::tmp_dir() ).arg( m_sTestKitNameSampleFormats )
 		.arg( Filesystem::drumkit_ext );
 	QTemporaryDir exportValidation( H2Core::Filesystem::tmp_dir() + "-XXXXXX" );
 	exportValidation.setAutoRemove( false );
