9291e0d18c4efb90c2d83565a591ec58e6e730c5
theGreatWhiteShark <princess.trudildis@posteo.de>
2024-11-04 22:07:28 +0100

    PatternEditor: properly initialize resolution
    and triplet usage

--- src/gui/src/PatternEditor/PatternEditor.cpp
+++ src/gui/src/PatternEditor/PatternEditor.cpp
@@ -57,8 +57,6 @@ PatternEditor::PatternEditor( QWidget *pParent,
 	, QWidget( pParent )
 	, m_selection( this )
 	, m_bEntered( false )
-	, m_nResolution( 8 )
-	, m_bUseTriplets( false )
 	, m_pDraggedNote( nullptr )
 	, m_pPatternEditorPanel( panel )
 	, m_pPattern( nullptr )
@@ -70,8 +68,10 @@ PatternEditor::PatternEditor( QWidget *pParent,
 	, m_mode( Mode::None )
 {
 
-	auto pPref = H2Core::Preferences::get_instance();
-	
+	const auto pPref = H2Core::Preferences::get_instance();
+
+	m_nResolution = pPref->getPatternEditorGridResolution();
+	m_bUseTriplets = pPref->isPatternEditorUsingTriplets();
 	m_fGridWidth = pPref->getPatternEditorGridWidth();
 	m_nEditorWidth = PatternEditor::nMargin + m_fGridWidth * ( MAX_NOTES * 4 );
 	m_nActiveWidth = m_nEditorWidth;
