8874417082a2fcc31e47ca3f50f4f229eb255661
theGreatWhiteShark <princess.trudildis@posteo.de>
2025-09-29 21:58:19 +0200

    TimeHelper: first draft of burn in
    we now measure the surplus time the system requires to recover from a sleep, use the maximum of the last 100 measurements (we are conservative in here), and take it into account during the next sleep

--- src/core/Helpers/TimeHelper.cpp
+++ src/core/Helpers/TimeHelper.cpp
@@ -21,36 +21,116 @@
  */
 
 #include "TimeHelper.h"
-
+#include <algorithm>
+#include <chrono>
+#include <locale>
+#include <mutex>
 #include <thread>
 
 namespace H2Core {
 
-	TimeHelper::TimeHelper() {
-	}
-	TimeHelper::~TimeHelper() {
+TimeHelper::TimeHelper() : m_nSleepSurplusIndex( 0 )
+						   , m_bIsBurnedIn( false )
+						   , m_bAbortBurnIn( false )
+{
+	m_sleepSurplusNs.resize( TimeHelper::nSurplusVectorLength );
+	for ( int ii = 0; ii < m_sleepSurplusNs.size(); ++ii ) {
+		m_sleepSurplusNs[ ii ] = 0;
 	}
 
-	void TimeHelper::highResolutionSleep(
-		std::chrono::duration<float, std::micro> interval )
+	m_pBurnInThread = std::make_shared< std::thread >(
+		TimeHelper::burnIn, (void*)this );
+}
+
+TimeHelper::~TimeHelper() {
+	if ( m_pBurnInThread != nullptr ) {
+		m_bAbortBurnIn = true;
+		m_pBurnInThread->join();
+	}
+}
+
+void TimeHelper::highResolutionSleep(
+	std::chrono::duration<float, std::micro> interval )
+{
+	const auto start = Clock::now();
+
+	// Giving up control and relying on the OS scheduler to retrieve it again is
+	// expensive. The C++ std method std::this_thread::sleep_for only guarantees
+	// to sleep for at least the provided amount. It could very sleep longer.
+	// And it does. To circumvent this problem, we ask it for sleeping a little
+	// bit less and just wait the remaining time.
+	long long nSleepSurplusNs = 0;
 	{
-		const auto start = Clock::now();
-
-		// Giving up control and relying on the OS scheduler to retrieve it
-		// again is expensive. The C++ std method std::this_thread::sleep_for
-		// only guarantees to sleep for at least the provided amount. It could
-		// very sleep longer. And it does. To circumvent this problem, we ask it
-		// for sleeping a little bit less and just wait the remaining time.
-		const std::chrono::duration<float, std::milli> residualInterval{ 5 };
-		if ( interval > residualInterval ) {
-			std::this_thread::sleep_for( interval - residualInterval );
-		}
-
-		// This is very expensive. We waste a lot of CPU by doing nothing. To
-		// make the time spent on this loop as small as possible is an
-		// appropriate optimization target.
-		while ( Clock::now() - start < interval ) {
-		}
+		std::scoped_lock lock{ m_sleepSurplusMutex };
+		nSleepSurplusNs = *std::max_element( m_sleepSurplusNs.begin(),
+											m_sleepSurplusNs.end() );
 	}
+
+	const std::chrono::duration<long long, std::nano> residualInterval{
+		nSleepSurplusNs };
+	DEBUGLOG( QString( "Max surplus: %1" ).arg( nSleepSurplusNs ) );
+	if ( interval > residualInterval ) {
+
+		// We measure the time require for sleeping to do some statistics and
+		// provide the best guess possible when calculating the next sleep
+		// interval.
+		const auto preSleep = Clock::now();
+		std::this_thread::sleep_for( interval - residualInterval );
+		const auto postSleep = Clock::now();
+
+		std::scoped_lock lock{ m_sleepSurplusMutex };
+		if ( m_nSleepSurplusIndex >= m_sleepSurplusNs.size() ) {
+			m_nSleepSurplusIndex = 0;
+		}
+		m_sleepSurplusNs[ m_nSleepSurplusIndex ] =
+			std::chrono::duration_cast<std::chrono::nanoseconds>(
+				postSleep - preSleep - interval + residualInterval ).count();
+		DEBUGLOG( QString( "Current surplus interval: %1" )
+.arg( m_sleepSurplusNs[ m_nSleepSurplusIndex ] ) );
+		++m_nSleepSurplusIndex;
+	}
+
+	// This is very expensive. We waste a lot of CPU by doing nothing. To make
+	// the time spent on this loop as small as possible is an appropriate
+	// optimization target.
+	while ( Clock::now() - start < interval ) {
+	}
+}
+
+void TimeHelper::burnIn( void* pInstance ) {
+	auto pTimeHelper = static_cast<TimeHelper*>( pInstance );
+	if ( pTimeHelper == nullptr ) {
+		ERRORLOG( "Invalid instance provided. Shutting down." );
+		return;
+	}
+
+	for ( int ii = 0; ii < TimeHelper::nSurplusVectorLength; ++ii ) {
+		if ( pTimeHelper->m_bAbortBurnIn ) {
+			return;
+		}
+		std::scoped_lock lock{ pTimeHelper->m_sleepSurplusMutex };
+		if ( pTimeHelper->m_bAbortBurnIn ) {
+			return;
+		}
+		const auto start = Clock::now();
+		std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
+		const auto end = Clock::now();
+
+		const auto sleepDuration = end - start - std::chrono::milliseconds( 1 );
+		pTimeHelper->m_sleepSurplusNs[ ii ] =
+			std::chrono::duration_cast<std::chrono::nanoseconds>(
+				sleepDuration ).count();
+	}
+
+	if ( pTimeHelper->m_bAbortBurnIn ) {
+		return;
+	}
+	std::scoped_lock lock{ pTimeHelper->m_sleepSurplusMutex };
+	if ( pTimeHelper->m_bAbortBurnIn ) {
+		return;
+	}
+	pTimeHelper->m_nSleepSurplusIndex = 0;
+	pTimeHelper->m_bIsBurnedIn = true;
+}
 
 };
--- src/core/Helpers/TimeHelper.h
+++ src/core/Helpers/TimeHelper.h
@@ -27,19 +27,64 @@
 #include <core/Object.h>
 
 #include <chrono>
+#include <memory>
+#include <mutex>
+#include <thread>
 
 namespace H2Core
 {
 
+/** Sleeping is quite complicated. Giving up control and relying on the OS
+	scheduler to retrieve it again is expensive. The C++ std method
+	std::this_thread::sleep_for only guarantees to sleep for at least the
+	provided amount. It could very well sleep longer. And it does. To circumvent
+	this problem, we ask it for sleeping a little bit less and just wait the
+	remaining time. */
 class TimeHelper : public Object<TimeHelper>
 {
 		H2_OBJECT(TimeHelper)
-	public:
-			TimeHelper();
-			~TimeHelper();
 
-			void highResolutionSleep(
-				std::chrono::duration<float, std::micro> interval );
-	};
+	static constexpr int nSurplusVectorLength = 100;
+
+public:
+	TimeHelper();
+	~TimeHelper();
+
+	void highResolutionSleep(
+		std::chrono::duration<float, std::micro> interval );
+
+	bool isBurnedIn() const;
+
+
+private:
+	/** Perform a number of sleeps in a separate thread to fill
+	 * #m_sleepSurplusNs in order to do some statistics in
+	 * highResolutionSleep(). */
+	static void burnIn( void* pInstance );
+	bool m_bIsBurnedIn;
+	bool m_bAbortBurnIn;
+
+	/** Measurements of the time surplus slept by the system.
+	 *
+	 * When requesting a sleep for X ns and the system does sleep for X + Y ns,
+     * the `Y` is stored in this vector. On the next highResolutionSleep(), it
+     * is taken into account when compensating for the surplus time the system
+     * may require during sleep to switch back the context to the calling
+     * thread. */
+	std::vector<long long> m_sleepSurplusNs;
+	int m_nSleepSurplusIndex;
+
+	/** Since this class is designed to work in parallel threads, we have to
+        guard the access ot #m_sleepSurplusNs in order to avoid race
+        conditions. */
+	std::mutex m_sleepSurplusMutex;
+	std::shared_ptr< std::thread > m_pBurnInThread;
+
+};
+
+inline bool TimeHelper::isBurnedIn() const {
+	return m_bIsBurnedIn;
+}
+
 };
 #endif
