7b80740d03902f772a2fa18d15d352101bf57d5b
theGreatWhiteShark <princess.trudildis@posteo.de>
2026-06-08 08:51:23 +0200

    Filesystem: fix sample path preparation
    this one had to be ported to support arbitrary drumkit locations as well

--- src/core/Helpers/Filesystem.cpp
+++ src/core/Helpers/Filesystem.cpp
@@ -950,13 +950,6 @@ QString Filesystem::prepareSamplePath(
 	const QString& sDrumkitPath
 )
 {
-	// Check whether the provided absolute sample path is located within a known
-	// drumkit directory.
-	int nIndexMatch = -1;
-	const QStringList drumkitFolders =
-		Hydrogen::get_instance()->getSoundLibraryDatabase()->getDrumkitFolders(
-		);
-
 	// Normalize paths using QFileInfo. This way we neither have to deal with
 	// duplicated separators nor with platform-dependent quirks.
 	const QString sSamplePathCleaned =
@@ -970,18 +963,9 @@ QString Filesystem::prepareSamplePath(
 	// sample file name. Thus, we have to make sure to just string paths belong
 	// to that very drumkit.
 	if ( sSamplePathCleaned.startsWith( sDrumkitDirCleaned ) ) {
-		for ( const auto& ssFolder : drumkitFolders ) {
-			if ( sSamplePathCleaned.startsWith( ssFolder ) ) {
-				nIndexMatch =
-					sSamplePathCleaned.indexOf( "/", ssFolder.size() ) + 1;
-				break;
-			}
-		}
-	}
-
-	if ( nIndexMatch >= 0 ) {
-		// Sample is located in a drumkit folder. Just return basename.
-		QString sShortenedPath =
+		const int nIndexMatch =
+			sSamplePathCleaned.indexOf( "/", sDrumkitDirCleaned.size() ) + 1;
+		const QString sShortenedPath =
 			sSamplePathCleaned.right( sSamplePathCleaned.size() - nIndexMatch );
 
 		return std::move( sShortenedPath );
